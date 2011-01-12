3181554659870860854c46655c47b3ec878539fe
Douglas Lovell <dclo@us.ibm.com>
2011-01-12 14:54:20 -0500

    improve formatting of scores

--- app/models/score.rb
+++ app/models/score.rb
@@ -3,4 +3,9 @@ class Score < ActiveRecord::Base
   belongs_to :judge
 
   serialize :values
+
+  # convert the value * 10 into a pretty decimal
+  def self.display_score(score)
+    sprintf('%0.1f', score.fdiv(10))
+  end
 end
