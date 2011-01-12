5fd33bd6e91b82ad7ea60519b5103f91dd9c189f
Douglas Lovell <dclo@us.ibm.com>
2011-01-12 16:52:41 -0500

    improve layout and display, especially the pilot index

--- app/controllers/pilots_controller.rb
+++ app/controllers/pilots_controller.rb
@@ -1,10 +1,9 @@
 class PilotsController < ApplicationController
   def index
-    @pilots = Member.find_by_sql("select distinct 
-      m.given_name, m.family_name, m.id, f.category
-      from pilot_flights p, members m, flights f, contests c
-      where p.pilot_id = m.id and p.flight_id = f.id and f.contest_id = c.id
-      order by c.start, m.family_name, m.given_name")
+    @pilots = Member.find_by_sql("select 
+      m.given_name, m.family_name, m.id
+      from members m where m.id in (select distinct pilot_id from pilot_flights)
+      order by  m.family_name, m.given_name")
   end
 
   def show
