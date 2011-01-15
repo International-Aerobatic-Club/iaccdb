72d64cd80e852404bca623bd7145849889c4aa9c
Douglas Lovell <doug@wbreeze.com>
2011-01-15 13:59:17 -0500

    fix version of sqlite3

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,38 +2,29 @@ set :application, 'cdb'
 set :user, 'wbreezec'
 set :domain, 'wbreeze.com'
 
-# file paths
-set :deploy_to, "~/rails/#{application}"
-
 role :app, domain
 role :web, domain
 role :db, domain, :primary => true
 
-default_environment['PATH']='~/bin:/usr/bin:/bin'
-
-set :scm, 'git'
-set :scm-command, 'git --exec-path=~/bin'
-set :repository,  "file://~/.git/#{application}.git"
-set :deploy_via, :copy
+set :scm, :git
+set :repository, "#{user}@#{domain}:~/.git/#{application}.git"
 set :branch, 'master'
+set :deploy_via, :remote_cache
+set :deploy_to, "~/rails/#{application}"
 set :scm_verbose, true
 set :use_sudo, false
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
-
- namespace :deploy do
-   task :start do ; end
-   task :stop do ; end
-   task :restart, :roles => :app, :except => { :no_release => true } do
-     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-   end
- end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
 
 after "deploy:update_code", :bundle_install
 desc "install prerequisite gems"
 task :bundle_install, :roles => :app do
-   run "cd #{release_path} && bundle install"
+  run "cd #{release_path} && bundle install"
 end
 
