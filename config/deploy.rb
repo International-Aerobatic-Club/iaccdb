ae48958cc0db68df21f4bbb046fcc84e65579323
Douglas Lovell <doug@wbreeze.com>
2011-01-15 19:05:33 -0500

    redeploy on iaccdb.org

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,13 @@
 set :application, 'cdb'
-set :user, 'wbreezec'
-set :domain, 'wbreeze.com'
+set :user, 'iaccdbor'
+set :domain, 'iaccdb.org'
 
 role :app, domain
 role :web, domain
 role :db, domain, :primary => true
 
 set :scm, :git
-set :repository, "#{user}@#{domain}:~/.git/#{application}.git"
+set :repository, "#{user}@#{domain}:~/git/#{application}.git"
 set :branch, 'master'
 set :deploy_via, :remote_cache
 set :deploy_to, "~/rails/#{application}"
@@ -25,6 +25,7 @@ end
 after "deploy:update_code", :bundle_install
 desc "install prerequisite gems"
 task :bundle_install, :roles => :app do
+  puts "Shared path is #{shared_path}"
   run "cd #{release_path} && bundle install"
 end
 
