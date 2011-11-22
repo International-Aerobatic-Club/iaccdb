7df9e3e6e4236b31b8ddb3cac03f837a587515e7
Douglas Lovell <doug@wbreeze.com>
2011-11-22 10:52:25 -0500

    progress for ranks computations

--- lib/iac/rank_computer.rb
+++ lib/iac/rank_computer.rb
@@ -10,34 +10,36 @@ require 'ranking/computer.rb'
 # It completes rank information for the individual pilot-judge-figure
 # grades.
 module IAC
-class RankComputer
+  class RankComputer
 
-# Compute result values for one flight of the contest
-# Accepts a flight
-# Creates or updates pfj_result, pf_result
-# Does no computation if there are no sequence figure k values 
-# Returns the flight
-def self.computeFlight(flight)
-  apf = []
-  flight.pilot_flights.each do |pilot_flight|
-    apf << pilot_flight.results
-  end
-  fv = []
-  afv = []
-  apf.each do |pf|
-    fv << pf.flight_value
-    afv << pf.adj_flight_value
-  end
-  rv = Ranks::Computer.ranks_for(fv)
-  arv = Ranks::Computer.ranks_for(afv)
-  apf.each_with_index do |pf, i|
-    pf.flight_rank = rv[i]
-    pf.adj_flight_rank = arv[i]
-  end
-  flight
-end
+    # Compute result values for one flight of the contest
+    # Accepts a flight
+    # Creates or updates pfj_result, pf_result
+    # Does no computation if there are no sequence figure k values 
+    # Returns the flight
+    def self.computeFlight(flight)
+      apf = []
+      flight.pilot_flights.each do |pilot_flight|
+        apf << pilot_flight.results
+      end
+      fv = []
+      afv = []
+      apf.each do |pf|
+        fv << pf.flight_value
+        afv << pf.adj_flight_value
+      end
+      rv = Ranking::Computer.ranks_for(fv)
+      arv = Ranking::Computer.ranks_for(afv)
+      apf.each_with_index do |pf, i|
+        pf.flight_rank = rv[i]
+        pf.adj_flight_rank = arv[i]
+      end
+      flight
+    end
 
-###
-private
-###
+  ###
+  private
+  ###
 
+  end # class
+end # module
--- spec/model/rank_computer_spec.rb
+++ spec/model/rank_computer_spec.rb
@@ -1,121 +1,133 @@
 
 require 'spec_helper'
 
-module Model
+module IAC
   describe RankComputer do
     before(:each) do
-      seq = Factory.create(:sequence, :k_values => [2,2,2,2,2,2])
+      @contest = Factory.create(:contest)
+      @flight = Factory.create(:flight, :contest => @contest)
+      seq = Factory.create(:sequence, :k_values => [2,2,2,2,2])
       @pilot_flights = []
-      5.times { @pilot_flights << Factory.create(:pilot_flight, :sequence => seq)
+      5.times { @pilot_flights << Factory.create(:pilot_flight, 
+        :flight => @flight, :sequence => seq) }
       @judges = []
-      5.times { @judges << Factory.create(:judge) }
+      4.times { @judges << Factory.create(:judge) }
       # pilot_flights[0]
+      #1   9.0   9.5   8.0   9.0
+      #2   8.0   8.0   7.5   7.5
+      #3   8.5   8.5   8.0   8.5
+      #4   7.5   9.0   7.5   8.5
+      #5   8.0   7.5   5.5   7.0
       Factory.create(:score,
         :pilot_flight => @pilot_flights[0],
         :judge => @judges[0],
-        :values => [60, 0, 60, 0, 80, 0])
+        :values => [90, 80, 85, 75, 80])
       Factory.create(:score,
         :pilot_flight => @pilot_flights[0],
         :judge => @judges[1],
-        :values => [-1, 0, 90, -1, -1, -1])
+        :values => [95, 80, 85, 90, 75])
       Factory.create(:score,
         :pilot_flight => @pilot_flights[0],
         :judge => @judges[2],
-        :values => [80, 60, 0, 70, -1, -1])
+        :values => [80, 75, 80, 75, 55])
       Factory.create(:score,
         :pilot_flight => @pilot_flights[0],
         :judge => @judges[3],
-        :values => [80, 70, 0, 80, 0, 0])
-      Factory.create(:score,
-        :pilot_flight => @pilot_flights[0],
-        :judge => @judges[4],
-        :values => [60, 80, 0, 60, 0, 0])
+        :values => [90, 75, 85, 85, 70])
       # pilot_flights[1]
+      #1   7.0   8.0   6.5   6.5
+      #2   8.0   8.5   8.0   6.0
+      #3   8.5   9.0   6.0   8.0
+      #4   8.0   8.0   7.5   8.0
+      #5   7.0   7.0   5.0   7.5
       Factory.create(:score,
         :pilot_flight => @pilot_flights[1],
         :judge => @judges[0],
-        :values => [60, 0, 60, 0, 80, 0])
+        :values => [70, 80, 85, 80, 70])
       Factory.create(:score,
         :pilot_flight => @pilot_flights[1],
         :judge => @judges[1],
-        :values => [-1, 0, 90, -1, -1, -1])
+        :values => [80, 85, 90, 80, 70])
       Factory.create(:score,
         :pilot_flight => @pilot_flights[1],
         :judge => @judges[2],
-        :values => [80, 60, 0, 70, -1, -1])
+        :values => [65, 80, 60, 75, 50])
       Factory.create(:score,
         :pilot_flight => @pilot_flights[1],
         :judge => @judges[3],
-        :values => [80, 70, 0, 80, 0, 0])
-      Factory.create(:score,
-        :pilot_flight => @pilot_flights[1],
-        :judge => @judges[4],
-        :values => [60, 80, 0, 60, 0, 0])
+        :values => [65, 60, 80, 80, 75])
       # pilot_flights[2]
+      #1   8.0   8.0   7.5   7.5
+      #2   3.0   6.0   5.0   3.0
+      #3   8.0   7.0   7.0   8.0
+      #4   8.5   8.0   8.0   8.5
+      #5   6.0   6.0   7.0   7.0
       Factory.create(:score,
         :pilot_flight => @pilot_flights[2],
         :judge => @judges[0],
-        :values => [60, 0, 60, 0, 80, 0])
+        :values => [80, 30, 80, 85, 60])
       Factory.create(:score,
         :pilot_flight => @pilot_flights[2],
         :judge => @judges[1],
-        :values => [-1, 0, 90, -1, -1, -1])
+        :values => [80, 60, 70, 80, 60])
       Factory.create(:score,
         :pilot_flight => @pilot_flights[2],
         :judge => @judges[2],
-        :values => [80, 60, 0, 70, -1, -1])
+        :values => [75, 50, 70, 80, 70])
       Factory.create(:score,
         :pilot_flight => @pilot_flights[2],
         :judge => @judges[3],
-        :values => [80, 70, 0, 80, 0, 0])
-      Factory.create(:score,
-        :pilot_flight => @pilot_flights[2],
-        :judge => @judges[4],
-        :values => [60, 80, 0, 60, 0, 0])
+        :values => [75, 30, 80, 85, 70])
       # pilot_flights[3]
+      #1   9.5   7.0   7.5   8.5
+      #2   8.5   8.5   7.5   8.5
+      #3   9.0   9.0   8.0   9.0
+      #4   9.0   8.5   7.5   8.5
+      #5   8.5   8.0   7.0   8.0
       Factory.create(:score,
         :pilot_flight => @pilot_flights[3],
         :judge => @judges[0],
-        :values => [60, 0, 60, 0, 80, 0])
+        :values => [95, 85, 90, 90, 85])
       Factory.create(:score,
         :pilot_flight => @pilot_flights[3],
         :judge => @judges[1],
-        :values => [-1, 0, 90, -1, -1, -1])
+        :values => [70, 85, 90, 85, 80])
       Factory.create(:score,
         :pilot_flight => @pilot_flights[3],
         :judge => @judges[2],
-        :values => [80, 60, 0, 70, -1, -1])
+        :values => [75, 75, 80, 75, 70])
       Factory.create(:score,
         :pilot_flight => @pilot_flights[3],
         :judge => @judges[3],
-        :values => [80, 70, 0, 80, 0, 0])
-      Factory.create(:score,
-        :pilot_flight => @pilot_flights[3],
-        :judge => @judges[4],
-        :values => [60, 80, 0, 60, 0, 0])
+        :values => [85, 85, 90, 85, 80])
       # pilot_flights[4]
+      #1   9.0   8.0   7.5   7.0
+      #2   8.5   8.5   8.0   8.0
+      #3   8.5   8.5   9.0   9.0
+      #4   9.0   9.0   8.5   9.0
+      #5   8.5   7.0   8.0   7.5
       Factory.create(:score,
         :pilot_flight => @pilot_flights[4],
         :judge => @judges[0],
-        :values => [60, 0, 60, 0, 80, 0])
+        :values => [90, 85, 85, 90, 85])
       Factory.create(:score,
         :pilot_flight => @pilot_flights[4],
         :judge => @judges[1],
-        :values => [-1, 0, 90, -1, -1, -1])
+        :values => [80, 85, 85, 90, 70])
       Factory.create(:score,
         :pilot_flight => @pilot_flights[4],
         :judge => @judges[2],
-        :values => [80, 60, 0, 70, -1, -1])
+        :values => [75, 80, 90, 85, 80])
       Factory.create(:score,
         :pilot_flight => @pilot_flights[4],
         :judge => @judges[3],
-        :values => [80, 70, 0, 80, 0, 0])
-      Factory.create(:score,
-        :pilot_flight => @pilot_flights[4],
-        :judge => @judges[4],
-        :values => [60, 80, 0, 60, 0, 0])
+        :values => [70, 80, 90, 90, 75])
+      @contest.results
     end
-    it 'computes and caches figure and flight values' do
+    it 'ranks pilots for a flight' do
+      [3, 4, 5, 2, 1].each_with_index do |r,i|
+        @pilot_flights[i].pf_results.first.flight_rank.should == r
+      end
     end
   end
 end
