f4a917a04b44768a67309d9675c0bddb39c0a934
Douglas Lovell <doug@wbreeze.com>
2012-01-04 22:11:39 -0500

    can view and delete contests and manny_synch records

---
+++ app/controllers/admin/manny_synchs_controller.rb
@@ -0,0 +1,15 @@
+class Admin::MannySynchsController < ApplicationController
+
+  def index
+    @manny_synchs = MannySynch.includes(:contest).order("synch_date DESC")
+  end
+
+  def destroy
+    @manny = MannySynch.find(params[:id])
+    @manny.contest.destroy if @manny.contest
+    @manny.destroy
+
+    redirect_to(admin_manny_synchs_url)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Iac::Application.routes.draw do
+
+  namespace :admin do
+    resources :contests, :except => [:new, :create]
+    resources :manny_synchs, :only => [:index, :destroy]
+  end
+
   get "pages/notes"
 
   resources :contests, :only => [:index, :show]
