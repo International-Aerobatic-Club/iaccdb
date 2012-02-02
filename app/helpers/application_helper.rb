ecdeb287bd8409ec65da4193f9b9ce76ecd53066
Douglas Lovell <doug@wbreeze.com>
2012-02-02 12:23:59 -0500

    protect against nil printing floats

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def decimal_two(v)
+    sprintf('%.02f', v  ? v : 0)
+  end
+
+  def score_two(v)
+    v ||= 0
+    decimal_two(v.fdiv(10))
+  end
+
+  def score_pct_two(n,d)
+    n ||= 0
+    d ||= 1
+    decimal_two((n * 10.0).fdiv(d))
+  end
+
+  def k_score_two(k)
+    k ||= 0
+    decimal_two(k * 10)
+  end
 end
