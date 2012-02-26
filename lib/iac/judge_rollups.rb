241ca2a400c5642e007ffef2b8ed428a7b24ea29
Douglas Lovell <doug@wbreeze.com>
2012-02-26 15:50:38 -0500

    add additional judge metrics and rollup into year-judge-category

---
+++ lib/iac/judge_rollups.rb
@@ -0,0 +1,54 @@
+# assume loaded with rails ActiveRecord
+# environment for IAC contest data application
+
+module IAC
+
+# this class contains methods to compute jy_results entries for
+# all judges and categories in any given year
+class JudgeRollups
+
+# Accepts the year
+# Computes or recomputes jy_results for every judge in every category
+def self.compute_jy_results (year)
+  # start_list = list all jy_result where year
+  start_list = JyResult.find_all_by_year(year)
+  puts "start with #{start_list}"
+  # all contests where year(start) == year
+  Contest.where(["year(start) = ?", year]).each do |contest|
+    # all c_results
+    puts "add #{contest.year_name} to judge rollups"
+    contest.c_results.each do |c_result|
+      # get category
+      puts "add #{c_result.display_category} to judge rollups"
+      category = Category.find_by_category_and_aircat(c_result.category,
+        c_result.aircat)
+      if category
+        # all jc_result
+        c_result.jc_results.each do |jc_result|
+          # find or create jy_result for year, category, judge
+          puts "Category #{category.name}, Judge #{jc_result.judge.name}"
+          jy_result  =
+          JyResult.find_or_initialize_by_judge_id_and_category_id_and_year(
+            jc_result.judge.id, category.id, year)
+          # if in start_list, reset & remove from start_list
+          if (start_list.include?(jy_result))
+            jy_result.zero_reset
+            start_list.delete(jy_result)
+          end
+          # combine jc_result
+          jy_result.accumulate(jc_result)
+          jy_result.save
+        end
+      else
+        puts("failed category #{c_result.display_category} for contest #{contest.year_name}")
+      end
+    end
+  end
+  # destroy any jy_result still in start_list
+  start_list.each do |jy_result|
+    jy_result.destroy
+  end
+end
+
+end #class
+end #module
