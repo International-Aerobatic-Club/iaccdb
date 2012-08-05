d6f1c482ffbbcd6fd8d29fa608185e530f35a86d
Douglas Lovell <doug@wbreeze.com>
2012-08-05 16:26:49 -0400

    correct processing of retrieved data does not join two separate records when a block boundary coincides with a record boundary. simplified processing.

--- lib/iac/manny_connect.rb
+++ lib/iac/manny_connect.rb
@@ -7,16 +7,10 @@ MANNY_PORT = 1001
 
 def pull_contest(manny_number, line_proc)
   query = "<ContestDetail><ContestID>#{manny_number}</ContestID></ContestDetail>"
-  tail = ''
   Net::HTTP.start(MANNY_HOST, MANNY_PORT) do |http|
-    http.post('/', query, {'Content-Type' => 'text/xml'}) do |data| 
-      data = tail << data
-      aRcd = data.split("\r")
-      if !aRcd.empty?
-        tail = aRcd.last
-        aRcd.pop
-        aRcd.each { |rcd| line_proc.call(rcd) }
-      end
+    resp = http.post('/', query, {'Content-Type' => 'text/xml'})
+    if resp.kind_of?(Net::HTTPSuccess)
+      resp.body.each_line { |line| line_proc.call(line) }
     end
   end
 end
@@ -24,17 +18,11 @@ end
 def pull_contest_list(record_proc)
   tail = ''
   Net::HTTP.start(MANNY_HOST, MANNY_PORT) do |http|
-    http.post('/', '<ContestList/>', {'Content-Type' => 'text/xml'}) do |data| 
-      data = tail << data
-      aRcd = data.split("\r")
-      if !aRcd.empty?
-        tail = aRcd.last
-        aRcd.pop
-        aRcd.each { |rcd| record_proc.call(rcd) }
-      end
+    resp = http.post('/', '<ContestList/>', {'Content-Type' => 'text/xml'})
+    if resp.kind_of?(Net::HTTPSuccess)
+      resp.body.each_line { |line| record_proc.call(line) }
     end
   end
-  # tail should be empty because data ends with a record delimiter
 end
 
 end
