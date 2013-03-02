1cec2e298ff8496289aef029fc1d2d07a00e7ad7
Douglas Lovell <doug@wbreeze.com>
2013-03-01 21:01:23 -0500

    return from document.find is a list

--- lib/iac/jasper_parse.rb
+++ lib/iac/jasper_parse.rb
@@ -7,19 +7,19 @@ class JaSPerParse
   end
 
   def contest_id
-    node = @document.find('/ContestResults/ContestInfo/Id')
-    cid = node ? node.first.inner_xml : nil
+    nodes = @document.find('/ContestResults/ContestInfo/cdbID')
+    cid = nodes && nodes.first ? nodes.first.inner_xml : nil
     cid = cid.to_i if cid
   end
 
   def contest_name
-    node = @document.find('/ContestResults/ContestInfo/Contest')
-    node ? node.first.inner_xml : ''
+    nodes = @document.find('/ContestResults/ContestInfo/Contest')
+    nodes && nodes.first ? nodes.first.inner_xml : ''
   end
 
   def contest_date
-    node = @document.find('/ContestResults/ContestInfo/Date')
-    text = node ? node.first.inner_xml : nil
+    nodes = @document.find('/ContestResults/ContestInfo/Date')
+    text = nodes && nodes.first ? nodes.first.inner_xml : nil
     if text
       Date.strptime(text, '%m/%d/%y')
     else
@@ -28,33 +28,33 @@ class JaSPerParse
   end
 
   def contest_region
-    node = @document.find('/ContestResults/ContestInfo/Region')
-    node ? node.first.inner_xml : ''
+    nodes = @document.find('/ContestResults/ContestInfo/Region')
+    nodes && nodes.first ? nodes.first.inner_xml : ''
   end
 
   def contest_director
-    node = @document.find('/ContestResults/ContestInfo/Director')
-    node ? node.first.inner_xml : ''
+    nodes = @document.find('/ContestResults/ContestInfo/Director')
+    nodes && nodes.first ? nodes.first.inner_xml : ''
   end
 
   def contest_city
-    node = @document.find('/ContestResults/ContestInfo/City')
-    node ? node.first.inner_xml : ''
+    nodes = @document.find('/ContestResults/ContestInfo/City')
+    nodes && nodes.first ? nodes.first.inner_xml : ''
   end
 
   def contest_state
-    node = @document.find('/ContestResults/ContestInfo/State')
-    node ? node.first.inner_xml : ''
+    nodes = @document.find('/ContestResults/ContestInfo/State')
+    nodes && nodes.first ? nodes.first.inner_xml : ''
   end
 
   def contest_chapter
-    node = @document.find('/ContestResults/ContestInfo/HostChapter')
-    node ? node.first.inner_xml : ''
+    nodes = @document.find('/ContestResults/ContestInfo/HostChapter')
+    nodes && nodes.first ? nodes.first.inner_xml : ''
   end
 
   def aircat
-    node = @document.find('/ContestResults/ContestInfo/Type')
-    type = node ? node.first.inner_xml : 'Powered'
+    nodes = @document.find('/ContestResults/ContestInfo/Type')
+    type = nodes && nodes.first ? nodes.first.inner_xml : 'Powered'
     type[0,1]
   end
 
@@ -98,8 +98,7 @@ class JaSPerParse
 
   def pilots_scored(jCat, jFlt)
     pIDList = []
-    nodes =
-    @document.find("/ContestResults/Scores/Category[@CategoryID=#{jCat}]/Flight[@FlightID=#{jFlt}]/Pilot")
+    nodes = @document.find("/ContestResults/Scores/Category[@CategoryID=#{jCat}]/Flight[@FlightID=#{jFlt}]/Pilot")
     nodes.each do |node|
       attribs = node.attributes
       attr = attribs.get_attribute('PilotID')
@@ -124,70 +123,60 @@ class JaSPerParse
   end
 
   def chief_iac_number(jCat, jFlt)
-    node =
-    @document.find("/ContestResults/Judges/Category[@CategoryID=#{jCat}]/Flight[@FlightID=#{jFlt}]/Judge[@JudgeID=0]/IACNumber")
-    node ? node.first.inner_xml : ''
+    nodes = @document.find("/ContestResults/Judges/Category[@CategoryID=#{jCat}]/Flight[@FlightID=#{jFlt}]/Judge[@JudgeID=0]/IACNumber")
+    nodes && nodes.first ? nodes.first.inner_xml : ''
   end
 
   def chief_first_name(jCat, jFlt)
-    node =
-    @document.find("/ContestResults/Judges/Category[@CategoryID=#{jCat}]/Flight[@FlightID=#{jFlt}]/Judge[@JudgeID=0]/Name/First")
-    node ? node.first.inner_xml : ''
+    nodes = @document.find("/ContestResults/Judges/Category[@CategoryID=#{jCat}]/Flight[@FlightID=#{jFlt}]/Judge[@JudgeID=0]/Name/First")
+    nodes && nodes.first ? nodes.first.inner_xml : ''
   end
 
   def chief_last_name(jCat, jFlt)
-    node =
-    @document.find("/ContestResults/Judges/Category[@CategoryID=#{jCat}]/Flight[@FlightID=#{jFlt}]/Judge[@JudgeID=0]/Name/Last")
-    node ? node.first.inner_xml : ''
+    nodes = @document.find("/ContestResults/Judges/Category[@CategoryID=#{jCat}]/Flight[@FlightID=#{jFlt}]/Judge[@JudgeID=0]/Name/Last")
+    nodes && nodes.first ? nodes.first.inner_xml : ''
   end
 
   def chief_assist_iac_number(jCat, jFlt)
-    node =
-    @document.find("/ContestResults/Judges/Category[@CategoryID=#{jCat}]/Flight[@FlightID=#{jFlt}]/Judge[@JudgeID=0]/Assistant[1]/IACNumber")
-    node ? node.first.inner_xml : ''
+    nodes = @document.find("/ContestResults/Judges/Category[@CategoryID=#{jCat}]/Flight[@FlightID=#{jFlt}]/Judge[@JudgeID=0]/Assistant[1]/IACNumber")
+    nodes && nodes.first ? nodes.first.inner_xml : ''
   end
 
   def chief_assist_first_name(jCat, jFlt)
-    node =
-    @document.find("/ContestResults/Judges/Category[@CategoryID=#{jCat}]/Flight[@FlightID=#{jFlt}]/Judge[@JudgeID=0]/Assistant[1]/Name/First")
-    node ? node.first.inner_xml : ''
+    nodes = @document.find("/ContestResults/Judges/Category[@CategoryID=#{jCat}]/Flight[@FlightID=#{jFlt}]/Judge[@JudgeID=0]/Assistant[1]/Name/First")
+    nodes && nodes.first ? nodes.first.inner_xml : ''
   end
 
   def chief_assist_last_name(jCat, jFlt)
-    node =
-    @document.find("/ContestResults/Judges/Category[@CategoryID=#{jCat}]/Flight[@FlightID=#{jFlt}]/Judge[@JudgeID=0]/Assistant[1]/Name/Last")
-    node ? node.first.inner_xml : ''
+    nodes = @document.find("/ContestResults/Judges/Category[@CategoryID=#{jCat}]/Flight[@FlightID=#{jFlt}]/Judge[@JudgeID=0]/Assistant[1]/Name/Last")
+    nodes && nodes.first ? nodes.first.inner_xml : ''
   end
 
   def pilot_iac_number(jCat, jPilot)
-    node =
-    @document.find("/ContestResults/Pilots/Category[@CategoryID=#{jCat}]/Pilot[@PilotID=#{jPilot}]/IACNumber")
-    node ? node.first.inner_xml : ''
+    nodes = @document.find("/ContestResults/Pilots/Category[@CategoryID=#{jCat}]/Pilot[@PilotID=#{jPilot}]/IACNumber")
+    nodes && nodes.first ? nodes.first.inner_xml : ''
   end
 
   def pilot_first_name(jCat, jPilot)
-    node =
-    @document.find("/ContestResults/Pilots/Category[@CategoryID=#{jCat}]/Pilot[@PilotID=#{jPilot}]/Name/First")
-    node ? node.first.inner_xml : ''
+    nodes = @document.find("/ContestResults/Pilots/Category[@CategoryID=#{jCat}]/Pilot[@PilotID=#{jPilot}]/Name/First")
+    nodes && nodes.first ? nodes.first.inner_xml : ''
   end
 
   def pilot_last_name(jCat, jPilot)
-    node =
-    @document.find("/ContestResults/Pilots/Category[@CategoryID=#{jCat}]/Pilot[@PilotID=#{jPilot}]/Name/Last")
-    node ? node.first.inner_xml : ''
+    nodes = @document.find("/ContestResults/Pilots/Category[@CategoryID=#{jCat}]/Pilot[@PilotID=#{jPilot}]/Name/Last")
+    nodes && nodes.first ? nodes.first.inner_xml : ''
   end
 
   def pilot_chapter(jCat, jPilot)
-    node =
-    @document.find("/ContestResults/Pilots/Category[@CategoryID=#{jCat}]/Pilot[@PilotID=#{jPilot}]/Chapter")
-    node ? node.first.inner_xml : ''
+    nodes = @document.find("/ContestResults/Pilots/Category[@CategoryID=#{jCat}]/Pilot[@PilotID=#{jPilot}]/Chapter")
+    nodes && nodes.first ? nodes.first.inner_xml : ''
   end
 
   def penalty(jCat, jFlt, jPilot)
-    node =
+    nodes =
     @document.find("/ContestResults/Scores/Category[@CategoryID=#{jCat}]/Flight[@FlightID=#{jFlt}]/Pilot[@PilotID=#{jPilot}]/Penalty")
-    if node && node.first
-      penalty_text = node.first.inner_xml
+    if nodes && nodes.first
+      penalty_text = nodes.first.inner_xml
       penalty_text.to_i
     else
       0
@@ -195,39 +184,39 @@ class JaSPerParse
   end
 
   def judge_iac_number(jCat, jFlt, jJudgeTeam)
-    node =
+    nodes =
     @document.find("/ContestResults/Judges/Category[@CategoryID=#{jCat}]/Flight[@FlightID=#{jFlt}]/Judge[@JudgeID=#{jJudgeTeam}]/IACNumber")
-    node ? node.first.inner_xml : ''
+    nodes && nodes.first ? nodes.first.inner_xml : ''
   end
 
   def judge_first_name(jCat, jFlt, jJudgeTeam)
-    node =
+    nodes =
     @document.find("/ContestResults/Judges/Category[@CategoryID=#{jCat}]/Flight[@FlightID=#{jFlt}]/Judge[@JudgeID=#{jJudgeTeam}]/Name/First")
-    node ? node.first.inner_xml : ''
+    nodes && nodes.first ? nodes.first.inner_xml : ''
   end
 
   def judge_last_name(jCat, jFlt, jJudgeTeam)
-    node =
+    nodes =
     @document.find("/ContestResults/Judges/Category[@CategoryID=#{jCat}]/Flight[@FlightID=#{jFlt}]/Judge[@JudgeID=#{jJudgeTeam}]/Name/Last")
-    node ? node.first.inner_xml : ''
+    nodes && nodes.first ? nodes.first.inner_xml : ''
   end
 
   def judge_assist_iac_number(jCat, jFlt, jJudgeTeam)
-    node =
+    nodes =
     @document.find("/ContestResults/Judges/Category[@CategoryID=#{jCat}]/Flight[@FlightID=#{jFlt}]/Judge[@JudgeID=#{jJudgeTeam}]/Assistant/IACNumber")
-    node ? node.first.inner_xml : ''
+    nodes && nodes.first ? nodes.first.inner_xml : ''
   end
 
   def judge_assist_first_name(jCat, jFlt, jJudgeTeam)
-    node =
+    nodes =
     @document.find("/ContestResults/Judges/Category[@CategoryID=#{jCat}]/Flight[@FlightID=#{jFlt}]/Judge[@JudgeID=#{jJudgeTeam}]/Assistant/Name/First")
-    node ? node.first.inner_xml : ''
+    nodes && nodes.first ? nodes.first.inner_xml : ''
   end
 
   def judge_assist_last_name(jCat, jFlt, jJudgeTeam)
-    node =
+    nodes =
     @document.find("/ContestResults/Judges/Category[@CategoryID=#{jCat}]/Flight[@FlightID=#{jFlt}]/Judge[@JudgeID=#{jJudgeTeam}]/Assistant/Name/Last")
-    node ? node.first.inner_xml : ''
+    nodes && nodes.first ? nodes.first.inner_xml : ''
   end
 
   def grades_for(jCat, jFlt, jPilot, jJudgeTeam)
