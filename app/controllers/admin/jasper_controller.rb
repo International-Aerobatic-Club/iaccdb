1b7a032638c5cee31975b707802ebff931f1b364
Douglas Lovell <doug@wbreeze.com>
2013-05-07 12:43:19 -0400

    enable send data as string param as well as file attachment param

--- app/controllers/admin/jasper_controller.rb
+++ app/controllers/admin/jasper_controller.rb
@@ -4,24 +4,40 @@ class Admin::JasperController < ApplicationController
 
 def results
   xml_data = params[:contest_xml]
+  jasper = Jasper::JasperParse.new
   if xml_data.respond_to? :read
     begin
-      jasper = Jasper::JasperParse.new
-      j2d = Jasper::JasperToDB.new
       parser = LibXML::XML::Parser.io(xml_data)
       jasper.do_parse(parser)
-      contest = j2d.process_contest(jasper)
-      if (contest)
-        @contestID = contest.id
-        puts "admin/jasper_controller has #{@contestID}"
-        render :results
-      else
-        render :exception, :status => 400
-      end
     rescue Exception => ex
-      @exception = ex
+      @exception = ex.message
       render :exception, :status => 400
     end
+  elsif xml_data.is_a?(String)
+    begin
+      parser = LibXML::XML::Parser.string(xml_data)
+      jasper.do_parse(parser)
+    rescue Exception => ex
+      @exception = ex.message
+      render :exception, :status => 400
+    end
+  else
+    @exception = "invalid post"
+    render :exception, :status => 400
+  end
+  begin
+    j2d = Jasper::JasperToDB.new
+    contest = j2d.process_contest(jasper)
+    if (contest.save)
+      @contestID = contest.id
+      render :results
+    else
+      @exception = "failed to save"
+      render :exception, :status => 400
+    end
+  rescue Exception => ex
+    @exception = ex.message
+    render :exception, :status => 400
   end
 end
 
