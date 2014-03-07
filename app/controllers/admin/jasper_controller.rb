f297f3a1f746e2209b2766688312e12d6512dcfa
Douglas Lovell <doug@wbreeze.com>
2014-03-06 19:29:38 -0500

    add some logging and return 500 on any error after successful parse.

--- app/controllers/admin/jasper_controller.rb
+++ app/controllers/admin/jasper_controller.rb
@@ -7,17 +7,23 @@ def results
   jasper = Jasper::JasperParse.new
   if xml_data.respond_to? :read
     begin
+      logger.info "JasperController: parse xml file"
       parser = LibXML::XML::Parser.io(xml_data)
       jasper.do_parse(parser)
     rescue Exception => ex
+      logger.error ex.inspect
+      logger.error ex.backtrace.join('\n')
       @exception = ex.message
       render :exception, :status => 400
     end
   elsif xml_data.is_a?(String)
     begin
+      logger.info "JasperController: parse xml param"
       parser = LibXML::XML::Parser.string(xml_data)
       jasper.do_parse(parser)
     rescue Exception => ex
+      logger.error ex.inspect
+      logger.error ex.backtrace.join('\n')
       @exception = ex.message
       render :exception, :status => 400
     end
@@ -26,6 +32,7 @@ def results
     render :exception, :status => 400
   end
   begin
+    logger.info "JasperController: process xml to database"
     j2d = Jasper::JasperToDB.new
     contest = j2d.process_contest(jasper)
     if (contest.save)
@@ -33,11 +40,13 @@ def results
       render :results
     else
       @exception = "failed to save"
-      render :exception, :status => 400
+      render :exception, :status => 500
     end
   rescue Exception => ex
+    logger.error ex.inspect
+    logger.error ex.backtrace.join('\n')
     @exception = ex.message
-    render :exception, :status => 400
+    render :exception, :status => 500
   end
 end
 
