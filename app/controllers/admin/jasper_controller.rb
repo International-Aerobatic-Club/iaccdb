cbe70d7749d7a4b6c6a7ddd784033dd362794c48
Douglas Lovell <doug@wbreeze.com>
2014-03-20 17:10:29 -0400

    record raw post data for replay; create contest if necessary and determine id for return, but do not otherwise try to update the database

--- app/controllers/admin/jasper_controller.rb
+++ app/controllers/admin/jasper_controller.rb
@@ -3,50 +3,53 @@ require 'libxml'
 class Admin::JasperController < ApplicationController
 
 def results
-  xml_data = params[:contest_xml]
-  jasper = Jasper::JasperParse.new
-  if xml_data.respond_to? :read
-    begin
-      logger.info "JasperController: parse xml file"
-      parser = LibXML::XML::Parser.io(xml_data)
-      jasper.do_parse(parser)
-    rescue Exception => ex
-      logger.error ex.inspect
-      logger.error ex.backtrace.join('\n')
-      @exception = ex.message
-      render :exception, :status => 400
-    end
-  elsif xml_data.is_a?(String)
-    begin
-      logger.info "JasperController: parse xml param"
-      parser = LibXML::XML::Parser.string(xml_data)
-      jasper.do_parse(parser)
-    rescue Exception => ex
-      logger.error ex.inspect
-      logger.error ex.backtrace.join('\n')
-      @exception = ex.message
-      render :exception, :status => 400
-    end
-  else
-    @exception = "invalid post"
-    render :exception, :status => 400
-  end
+  @exception = nil
+  status = 200
   begin
-    logger.info "JasperController: process xml to database"
-    j2d = Jasper::JasperToDB.new
-    contest = j2d.process_contest(jasper)
-    if (contest.save)
-      @contestID = contest.id
-      render :results
+    xml_data = params[:contest_xml]
+    post_record = DataPost.new
+    post_record.save
+    if xml_data.respond_to? :read
+        logger.info "JasperController: read xml file"
+        post_record.data = xml_data.read
+        post_record.save
+    elsif xml_data.is_a?(String)
+        logger.info "JasperController: parse xml param"
+        post_record.data = xml_data
+        post_record.save
     else
-      @exception = "failed to save"
-      render :exception, :status => 500
+      @exception = "invalid post"
+      status = 400
+      post_record.has_error = true
+      post_record.error_description = @exception
+      post_record.save
+    end
+    if (@exception == nil)
+      parser = LibXML::XML::Parser.string(post_record.data)
+      jasper = Jasper::JasperParse.new
+      jasper.do_parse(parser)
+      @contest_id = jasper.contest_id
+      if (@contest_id == nil)
+        j2d = Jasper::JasperToDB.new
+        contest = Contest.create j2d.extract_contest_params_hash(jasper)
+        @contestID = contest.id
+      end
+      post_record.contest_id = @contest_id
+      post_record.save
     end
   rescue Exception => ex
+    @exception = ex.message
+    status = 500
     logger.error ex.inspect
     logger.error ex.backtrace.join('\n')
-    @exception = ex.message
-    render :exception, :status => 500
+    post_record.has_error = true
+    post_record.error_description = @exception
+    post_record.save
+  end
+  if (@exception == nil)
+    render :results
+  else
+    render :exception, :status => status
   end
 end
 
