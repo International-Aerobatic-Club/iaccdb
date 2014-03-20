ba1474460dec1b6225ae96d3921bfe08e63c460b
Douglas Lovell <doug@wbreeze.com>
2014-03-20 17:01:51 -0400

    add defaults

--- db/migrate/20140320180548_create_data_posts.rb
+++ db/migrate/20140320180548_create_data_posts.rb
@@ -3,10 +3,10 @@ class CreateDataPosts < ActiveRecord::Migration
     create_table :data_posts do |t|
       t.integer :contest_id
       t.text :data
-      t.boolean :is_integrated
-      t.boolean :has_error
+      t.boolean :is_integrated, :default => false
+      t.boolean :has_error, :default => false
       t.string :error_description
-      t.boolean :is_obsolete
+      t.boolean :is_obsolete, :default => false
 
       t.timestamps
     end
