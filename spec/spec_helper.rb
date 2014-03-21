97b6502f8cd86e106ccdd0f90b03713eff825848
Douglas Lovell <doug@wbreeze.com>
2014-03-21 10:18:09 -0400

    load seed data for categories

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,8 @@ require 'rspec/rails'
 RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.clean_with :truncation
+    load "#{Rails.root}/db/seeds.rb"
   end
 
   config.before(:each) do
