854e9134dbd2686197c44c6507edb786f8956984
Douglas Lovell <doug@wbreeze.com>
2014-03-22 14:31:58 -0400

    include the FactoryGirl helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     clean_db
