7a9d9a092a60e175df3340a1efbbbe3564ee7dd0
Douglas Lovell <doug@wbreeze.com>
2014-03-23 11:38:19 -0400

    show HZ, CA, A

--- app/models/score.rb
+++ app/models/score.rb
@@ -15,6 +15,14 @@ class Score < ActiveRecord::Base
 
   # convert the value * 10 into a pretty decimal
   def self.display_score(score)
-    score < 0 ? 'A' : sprintf('%0.1f', score.fdiv(10))
+    if score <= -30
+      'HZ'
+    elsif score <= -20
+      'CA'
+    elsif score < 0
+      'A'
+    else
+      sprintf('%0.1f', score.fdiv(10))
+    end
   end
 end
