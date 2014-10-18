b67116f7bc58f0af3dda92fb7a511908aeee343a
Douglas Lovell <doug@wbreeze.com>
2014-10-17 23:23:47 -0400

    display the flight descriptions scraped from the contest along with their identifying attributes, as a check

---
+++ cmd/show_flights.rb
@@ -0,0 +1,12 @@
+# no need to use rails runner
+# use ruby cmd/show_flights.rb <file>
+require_relative File.expand_path('../../config/environment', __FILE__)
+
+args = ARGV
+ctlFile = args[0]
+if (ctlFile)
+  contest_flights = ACRO::ContestFlights.new(ctlFile)
+  contest_flights.show_flights
+else
+  puts 'Supply the name of the contest information file'
+end
