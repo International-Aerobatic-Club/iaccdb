96a2ecf17a9afbe667d3e139414b76726b93956e
Douglas Lovell <doug@wbreeze.com>
2014-10-21 11:22:23 -0400

    reduce number of queries, simplify, add pilots

--- app/controllers/further_controller.rb
+++ app/controllers/further_controller.rb
@@ -1,30 +1,31 @@
 class FurtherController < ApplicationController
   def participation
-    judge_counts = JyResult.includes(:category).select(
-        'count(distinct(judge_id)) as count, category_id , year'
+    judge_counts = JyResult.joins(:category).select(
+        'count(distinct(judge_id)) as count, categories.id as category_id, year'
       ).group(:category_id, :year)
-    @cat_year_judge_count = judge_counts.group_by { |count| count.category }
+    @cat_year_judge_count = judge_counts.group_by { |count| count.category_id }
     @cat_year_judge_count.each do |key, ac| 
       @cat_year_judge_count[key] = ac.group_by { |count| count.year }
     end
     yy = judge_counts.collect { |jy_result| jy_result.year }
     @years = Set.new(yy)
+    judge_counts = JyResult.select('count(distinct(judge_id)) as count, year').group(:year)
+    @cat_year_judge_count['any'] = judge_counts.group_by { |count| count.year }
 
-    pilot_counts = PcResult.select(
-        'count(distinct(pilot_id)) as count, c_result_id'
-      ).group(:c_result_id)
-    @cat_year_pilot_count = pilot_counts.group_by { |pcr| pcr.category }
+    pilot_counts = PilotFlight.joins(:flight => [:category, :contest]).select(
+      'count(distinct(`pilot_flights`.`pilot_id`)) as count, categories.id as category_id, 
+       year(`contests`.`start`) as year').group(:category_id, :year)
+    @cat_year_pilot_count = pilot_counts.group_by { |pf| pf.category_id }
     @cat_year_pilot_count.each do |key, apcr| 
-      a_counts = apcr.group_by { |pcr| pcr.contest.year }
-      a_counts.each do |year, aapcr|
-        count = aapcr.inject(0) { |count, pcr| count += pcr.count }
-        aapcr[0].count = count
-        a_counts[year] = aapcr[0,1]
-      end
-      @cat_year_pilot_count[key] = a_counts
+      @cat_year_pilot_count[key] = apcr.group_by { |pcr| pcr.year }
     end
-    yy = pilot_counts.collect { |pf| pf.contest.year }
+    yy = pilot_counts.collect { |pf| pf.year }
     @years.merge(yy)
+    pilot_counts = PilotFlight.joins(:flight => :contest).select(
+      'count(distinct(`pilot_flights`.`pilot_id`)) as count, 
+       year(`contests`.`start`) as year').group(:year)
+    @cat_year_pilot_count['any'] = pilot_counts.group_by { |count| count.year }
+
     @categories = Category.order(:sequence)
   end
 end
