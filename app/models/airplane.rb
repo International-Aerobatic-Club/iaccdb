53a725c5a0eb2ed7ac11689b4c119f9e690e856e
Douglas Lovell <doug@wbreeze.com>
2014-10-29 22:01:07 -0400

    initialize airplane for pilot_flight

--- app/models/airplane.rb
+++ app/models/airplane.rb
@@ -19,6 +19,22 @@ class Airplane < ActiveRecord::Base
     plane
   end
 
+  # split combined make and model into separate make and model
+  # returns array [make, model]
+  def self.split_make_model(make_model)
+    make = model = nil
+    if make_model
+      amm = make_model.split(' ')
+      if (1 < amm.length)
+        model = amm.slice(1,make_model.length-1).join(' ')
+        make = amm[0]
+      else
+        model = amm[0]
+      end
+    end
+    [make, model]
+  end
+
   def description
     "#{make} #{model} #{reg}"
   end
--- spec/models/airplane_spec.rb
+++ spec/models/airplane_spec.rb
@@ -6,10 +6,23 @@ describe Airplane do
     model = '131'
     reg = 'CAXAL'
     airplane = Airplane.find_or_create_by_make_model_reg(make, model, reg)
-    airplane.reg.should == reg
-    airplane.make.should == make
-    airplane.model.should == model
+    expect(airplane.reg).to eq reg
+    expect(airplane.make).to eq make
+    expect(airplane.model).to eq model
     eAirplane = Airplane.find_or_create_by_make_model_reg(make, model, reg)
-    eAirplane.id.should == airplane.id
+    expect(eAirplane.id).to eq airplane.id
   end
+
+  it 'splits make model' do
+    mm = Airplane.split_make_model('Pitts S-1S')
+    expect(mm[0]).to eq 'Pitts'
+    expect(mm[1]).to eq 'S-1S'
+    mm = Airplane.split_make_model('Decathlon')
+    expect(mm[0]).to be_nil
+    expect(mm[1]).to eq 'Decathlon'
+    mm = Airplane.split_make_model('Extra 330 LX')
+    expect(mm[0]).to eq 'Extra'
+    expect(mm[1]).to eq '330 LX'
+  end
+
 end
