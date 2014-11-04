b66b8e6d1be08a9635c225d4078915221bc4aab2
Douglas Lovell <doug@wbreeze.com>
2014-11-03 23:06:17 -0500

    add soucy job; compute regional and soucy after rollups

---
+++ lib/jobs/compute_soucy_job.rb
@@ -0,0 +1,26 @@
+# This captures a job for delayed job
+# The job computes L. Paul Soucy results for a given year
+module Jobs
+class ComputeSoucyJob < Struct.new(:contest)
+  
+  include JobsSay
+
+  def perform
+    @contest = contest
+    @description = "#{contest.year}"
+    say "Computing L. Paul Soucy standings for #{@description}"
+    soucy = IAC::SoucyComputer.new(year)
+    soucy.recompute
+  end
+
+  def error(job, exception)
+    say "Error L. Paul Soucy computation #{@description}"
+    record_contest_failure(@description, @contest, exception)
+  end
+
+  def success(job)
+    say "Success L. Paul Soucy computation #{@description}"
+  end
+
+end
+end
