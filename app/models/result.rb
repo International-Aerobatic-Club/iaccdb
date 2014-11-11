c4ef234c0425c29197a2e5c19b61644b1a3973f2
Douglas Lovell <doug@wbreeze.com>
2014-11-11 13:27:47 -0500

    make members accessible and accept nested attributes

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,11 +1,13 @@
 class Result < ActiveRecord::Base
-  attr_accessible :category_id, :name, :pilot_id, :points, :points_possible, :qualified, :rank, :region, :type, :year
+  attr_accessible :category_id, :name, :pilot_id, :points, :points_possible, :qualified, :rank, :region, :type, :year, :members_attributes
   has_many :result_members
   has_many :members, :through => :result_members
   has_many :result_accums
   has_many :pc_results, :through => :result_accums
   belongs_to :pilot, :class_name => 'Member'
   belongs_to :category
+
+  accepts_nested_attributes_for :members, :allow_destroy => true
 
   def result_percent
     points_possible && points ? points * 100.0 / points_possible : 0.0
