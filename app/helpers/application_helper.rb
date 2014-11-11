ab00781503f6839d5ab1afdbaaa167a71256bd14
Douglas Lovell <doug@wbreeze.com>
2014-11-11 13:26:38 -0500

    helper provides link and form fields with magic identifiers for nested model edit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,4 +18,13 @@ module ApplicationHelper
     k ||= 0
     decimal_two(k * 10)
   end
+
+  def link_to_add_fields(name, form, association)
+    new_object = form.object.send(association).klass.new
+    id = new_object.object_id
+    fields = form.fields_for(association, new_object, child_index: id) do |fm|
+      render(association.to_s.singularize + '_fields', fm: fm)
+    end
+    link_to(name, '#', class: 'add_fields', data: {id: id, fields: fields.gsub("\n", '')})
+  end
 end
