3d55484421ed398ea030aa8cea2aa9fb756372a0
Douglas Lovell <doug@wbreeze.com>
2014-11-13 23:14:58 -0500

    begin code to compute collegiate results

---
+++ cmd/recompute_collegiate.rb
@@ -0,0 +1,11 @@
+# use rails runner cmd/recompute_collegiate.rb
+# will recompute collegiate team and individual results for given year
+
+year = ARGV.empty? ? 0 : ARGV[0].to_i
+if (0 < year)
+  cp = IAC::CollegiateComputer.new(year)
+  cp.recompute_individual
+  cp.recompute_team
+else
+  puts 'specify a year'
+end
