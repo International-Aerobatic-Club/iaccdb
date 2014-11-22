32e79a1a89aa3bcbbba2958c90165b1a1e685f8c
Douglas Lovell <doug@wbreeze.com>
2014-11-21 20:12:29 -0500

    document and correct individual result

--- app/models/collegiate_individual_result.rb
+++ app/models/collegiate_individual_result.rb
@@ -4,6 +4,7 @@ def compute_result
   best_total = 0.0
   best_possible = 0
   best_ratio = 0.0
+  have_non_primary = false
   results = pc_results.all
   if results.size < 3
     csz = results.size
@@ -15,11 +16,14 @@ def compute_result
   results.combination(csz) do |triple|
     total = triple.inject(0.0) { |t,r| t + r.category_value }
     possible = triple.inject(0) { |t,r| t + r.total_possible }
+    non_primary = triple.inject(false) { |t,r| t || !r.category.is_primary }
     ratio = 0 < possible ? total / possible : 0.0
-    if (best_ratio < ratio)
+    if ((non_primary && !have_non_primary) || best_ratio < ratio)
       best_total, best_possible, best_ratio = total, possible, ratio
+      have_non_primary ||= non_primary
     end
   end
+  self.qualified &&= have_non_primary
   self.points = best_total
   self.points_possible = best_possible
   save
