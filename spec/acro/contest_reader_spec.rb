8533b3d3c520f4f4ceef1e1134f5f7603f2cb312
Douglas Lovell <doug@wbreeze.com>
2014-11-29 19:43:06 -0500

    made all tests pass

--- spec/acro/contest_reader_spec.rb
+++ spec/acro/contest_reader_spec.rb
@@ -1,37 +1,32 @@
 module ACRO
+
   describe ContestReader do
     it 'creates a new contest' do
+      puts Contest.all
       ct = Contest.where(:start => '2011-09-25')
       ct.empty?.should == true
-      cs = ContestReader.new('spec/acro/newContest.yml')
+      cs = ContestReader.new(contest_data_file('newContest.yml'))
       ct = Contest.where(:start => '2011-09-25').first
-      cs.dContest.should == ct
+      cs.contest_record.should == ct
     end
 
     it 'finds an existing contest' do
-      ec = Factory(:existing_contest)
-      cs = ContestReader.new('spec/acro/existingContest.yml')
-      cs.dContest.should == ec
+      ec = create(:existing_contest)
+      cs = ContestReader.new(contest_data_file('existingContest.yml'))
+      cs.contest_record.should == ec
     end
 
     it 'finds missing data' do
       lambda { 
-        ContestReader.new('spec/acro/faultyContest.yml')
+        ContestReader.new(contest_data_file('faultyContest.yml'))
       }.should raise_error('Missing data for contest city')
-    end
-
-    it 'finds pilot data files' do
-      cs = ContestReader.new('spec/acro/newContest.yml')
-      cs.files.size.should == 7
-      File.exist?(cs.files.first).should == true
-      File.file?(cs.files.first).should == true
     end
 
     it 'creates judge member records' do
       md = Member.where(:given_name => 'Debby')
       md.empty?.should == true
-      cs = ContestReader.new('spec/acro/newContest.yml')
-      ps = PilotScraper.new('spec/acro/pilot_p002s17.htm')
+      cs = ContestReader.new(contest_data_file('newContest.yml'))
+      ps = PilotScraper.new(contest_data_file('pilot_p002s17.htm'))
       cs.process_pilotFlight(ps)
       mr = Member.where(:family_name => 'Rihn-Harvey').first
       md = Member.where(:given_name => 'Debby').first
@@ -39,11 +34,9 @@ module ACRO
     end
 
     it 'finds existing judge members' do
-      cs = ContestReader.new('spec/acro/newContest.yml')
-      cs.files.each do |pf|
-        ps = PilotScraper.new(pf)
-        cs.process_pilotFlight(ps)
-      end
+      cs = ContestReader.new(contest_data_file('newContest.yml'))
+      cs.read_contest
+      puts "Judges: #{Judge.all.join("\n")}"
       Judge.all.size.should == 18
       stols = Member.where(:family_name => 'Stoltenberg')
       stols.size.should == 1
@@ -54,8 +47,8 @@ module ACRO
     it 'creates pilot member records' do
       md = Member.where(:given_name => 'Kelly')
       md.empty?.should == true
-      cs = ContestReader.new('spec/acro/newContest.yml')
-      ps = PilotScraper.new('spec/acro/pilot_p002s17.htm')
+      cs = ContestReader.new(contest_data_file('newContest.yml'))
+      ps = PilotScraper.new(contest_data_file('pilot_p002s17.htm'))
       cs.process_pilotFlight(ps)
       mr = Member.where(:given_name => 'Kelly').first
       md = Member.where(:family_name => 'Adams').first
@@ -63,11 +56,8 @@ module ACRO
     end
 
     it 'finds existing pilot members' do
-      cs = ContestReader.new('spec/acro/newContest.yml')
-      cs.files.each do |pf|
-        ps = PilotScraper.new(pf)
-        cs.process_pilotFlight(ps)
-      end
+      cs = ContestReader.new(contest_data_file('newContest.yml'))
+      cs.read_contest
       ballews = Member.where(:family_name => 'Ballew').all
       ballews.size.should == 1
       apf = PilotFlight.where(:pilot_id => ballews.first)
@@ -77,15 +67,16 @@ module ACRO
     it 'creates flight records' do
       ct = Contest.where(:start => '2011-09-25')
       ct.empty?.should == true
-      cs = ContestReader.new('spec/acro/newContest.yml')
-      ps = PilotScraper.new('spec/acro/pilot_p002s17.htm')
+      cs = ContestReader.new(contest_data_file('newContest.yml'))
+      ps = PilotScraper.new(contest_data_file('pilot_p002s17.htm'))
       cs.process_pilotFlight(ps)
       ct = Contest.where(:start => '2011-09-25').first
       fla = ct.flights
       fla.size.should == 1
       fl = fla.first
-      fl.category.should == 'Advanced'
-      fl.aircat.should == 'P'
+      cat = fl.category
+      cat.category.should == 'advanced'
+      cat.aircat.should == 'P'
       fl.name.should == 'Free'
       pilot_flights = fl.pilot_flights
       pilot_flights.size.should == 1
@@ -104,11 +95,8 @@ module ACRO
     end
 
     it 'adds to existing flight records' do
-      cs = ContestReader.new('spec/acro/newContest.yml')
-      cs.files.each do |pf|
-        ps = PilotScraper.new(pf)
-        cs.process_pilotFlight(ps)
-      end
+      cs = ContestReader.new(contest_data_file('newContest.yml'))
+      cs.read_contest
       ct = Contest.where(:start => '2011-09-25').first
       fla = ct.flights
       fla.size.should == 4
@@ -116,14 +104,14 @@ module ACRO
     end
 
     it 'stores the penalty' do
-      cs = ContestReader.new('spec/acro/newContest.yml')
-      ps = PilotScraper.new('spec/acro/pilot_p002s17.htm')
+      cs = ContestReader.new(contest_data_file('newContest.yml'))
+      ps = PilotScraper.new(contest_data_file('pilot_p002s17.htm'))
       cs.process_pilotFlight(ps)
       ct = Contest.where(:start => '2011-09-25').first
       fl = ct.flights.first
       pf = fl.pilot_flights.first
       pf.penalty_total.should == 0
-      ps = PilotScraper.new('spec/acro/pilot_p002s16.htm')
+      ps = PilotScraper.new(contest_data_file('pilot_p002s16.htm'))
       cs.process_pilotFlight(ps)
       fl = ct.flights[1]
       pf = fl.pilot_flights.first
@@ -131,8 +119,8 @@ module ACRO
     end
 
     it 'stores figure k values' do
-      cs = ContestReader.new('spec/acro/newContest.yml')
-      ps = PilotScraper.new('spec/acro/pilot_p002s17.htm')
+      cs = ContestReader.new(contest_data_file('newContest.yml'))
+      ps = PilotScraper.new(contest_data_file('pilot_p002s17.htm'))
       cs.process_pilotFlight(ps)
       ct = Contest.where(:start => '2011-09-25').first
       fl = ct.flights.first
