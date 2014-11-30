8de3997f3c5b8bd418e3c5f9a77b337c65d3c413
Douglas Lovell <doug@wbreeze.com>
2014-11-30 09:35:05 -0500

    make post_dir available

--- app/models/data_post.rb
+++ app/models/data_post.rb
@@ -3,10 +3,14 @@ class DataPost < ActiveRecord::Base
 
   belongs_to :contest
 
+  def self.post_dir
+    File.join(::Rails.root.to_s, 'posts')
+  end
+
   def filename
-    post_dir = "#{::Rails.root.to_s}/posts"
+    post_dir = DataPost.post_dir
     if (!Dir.exist?(post_dir))
-      Dir.mkdir(post_dir)
+      Dir.mkdir(spost_dir)
     end
     File.join(post_dir,"JaSPer_post_#{id}.xml")
   end
