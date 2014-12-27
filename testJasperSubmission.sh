c0a2c86dfb9ee81ff004b2800ddcb286cb12a5fb
Douglas Lovell <doug@wbreeze.com>
2014-12-26 18:42:24 -0700

    use new location of test file

--- testJasperSubmission.sh
+++ testJasperSubmission.sh
@@ -1,1 +1,1 @@
-curl -F "contest_xml=@spec/jasper/jasperResultsFormat.xml" http://localhost:3000/admin/jasper.xml
+curl -F "contest_xml=@spec/fixtures/jasper/jasperResultsFormat.xml" http://localhost:3000/admin/jasper.xml
