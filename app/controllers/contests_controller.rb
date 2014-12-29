3c2245c9c399ea56c18dabc327a1ad0f1b860343
Douglas Lovell <doug@wbreeze.com>
2014-12-28 18:53:34 -0700

    use template builders for xml and json in place of separate render clauses

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -7,15 +7,6 @@ class ContestsController < ApplicationController
     @years.sort!{|a,b| b <=> a}
     @year = params[:year] || @years.first
     @contests = Contest.where('year(start) = ?', @year).order("start DESC")
-    respond_to do |format|
-      format.html
-      format.xml do
-        render xml: @contests.to_xml
-      end
-      format.json do
-        render json: @contests.as_json
-      end
-    end
   end
 
   # GET /contests/1
@@ -24,16 +15,7 @@ class ContestsController < ApplicationController
     @contest = Contest.find(params[:id])
     @c_results = @contest.c_results.sort { |a,b| a.category.sequence <=> b.category.sequence }
     if @c_results && !@c_results.empty?
-      respond_to do |format|
-        format.html
-          render :show
-        format.xml do
-          render xml: @contest.to_xml
-        end
-        format.json do
-          render json: @contest.as_json
-        end
-      end
+      render :show
     else
       render :raw, :notice => "Results unavailable"
     end
