60a8d950632c70eb19ef3763e29543aa7eda9dfc
Douglas Lovell <doug@wbreeze.com>
2015-10-26 21:51:52 -0600

    add script to recompute collegiate

---
+++ cmd/recompute_collegiate.rb
@@ -0,0 +1,10 @@
+# use rails runner cmd/recompute_collegiate.rb
+# will recompute collegiate results for given year
+
+year = ARGV.empty? ? 0 : ARGV[0].to_i
+if (0 < year)
+  collegiate_computer = IAC::CollegiateComputer.new(year)
+  collegiate_computer.recompute
+else
+  puts 'specify a year'
+end
