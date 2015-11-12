ba5289f08936c2db410384e45da76e5677c39946
Douglas Lovell <doug@wbreeze.com>
2015-11-12 02:02:03 -0700

    fix pilot scores display for no f_result

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -4,14 +4,10 @@ class ScoresController < ApplicationController
   def show
     @contest = Contest.find(params[:id])
     @pilot = Member.find(params[:pilot_id])
-    @pilot_flights = PilotFlight.find_by_sql(["select p.* 
-      from pilot_flights p, flights f 
-      where f.contest_id = :cid and p.flight_id = f.id and p.pilot_id = :pid
-      order by f.sequence", {:cid => params[:id], :pid => params[:pilot_id]}])
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @pilot_flights }
-    end
+    @pilot_flights = PilotFlight.joins(:flight).where(
+       {:pilot_flights => {pilot_id: @pilot},
+        :flights => {contest_id: @contest}}
+      ).order("flights.sequence")
   end
 
 end
