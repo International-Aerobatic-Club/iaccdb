6dde34713e9b391eef2822d9434ed377118cc92d
Douglas Lovell <doug@wbreeze.com>
2015-11-27 05:58:13 -0700

    fix failure test on compute_contest_rollups job

--- spec/jobs/compute_contest_rollups_job_spec.rb
+++ spec/jobs/compute_contest_rollups_job_spec.rb
@@ -1,24 +1,24 @@
 module Jobs
-describe ComputeContestRollupsJob do
-  before(:example) do
-    @contest = create(:contest)
-    @job = ComputeContestRollupsJob.new(@contest)
-  end
-
-  it 'creates and invokes the contest rollups' do
-    expect(ContestComputer).to receive(:new).once.with(@contest).and_call_original
-    @job.perform
-  end
-
-  it 'places an entry in the failure table on failure' do
-    allow(@contest).to receive(:compute_contest_rollups).and_raise Exception.new('failure')
-    begin
-      @job.perform
-    rescue Exception => e
-      @job.error(@job, e)
+  describe ComputeContestRollupsJob do
+    before(:example) do
+      @contest = create(:contest)
+      @job = ComputeContestRollupsJob.new(@contest)
     end
-    expect(Failure.first).not_to be nil
-  end
 
-end
+    it 'creates and invokes the contest rollups' do
+      expect(ContestComputer).to receive(:new).once.with(@contest).and_call_original
+      @job.perform
+    end
+
+    it 'places an entry in the failure table on failure' do
+      allow_any_instance_of(ContestComputer).to receive(
+        :compute_contest_rollups).and_raise Exception.new('failure')
+      begin
+        @job.perform
+      rescue Exception => e
+        @job.error(@job, e)
+      end
+      expect(Failure.first).not_to be nil
+    end
+  end
 end
