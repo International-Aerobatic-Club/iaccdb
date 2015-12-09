703c06924bb917554774d33321ebabdf028ff4bf
Douglas Lovell <doug@wbreeze.com>
2015-12-08 22:29:34 -0700

    add command to recompute judge metrics for year

--- cmd/recompute_judge_rollups.rb
+++ cmd/recompute_judge_rollups.rb
@@ -1,17 +1,19 @@
-# use rails runner lib/iac/recompute_judge_rollups.rb 
-# will check all of the contests
-require "iac/judge_rollups"
+# use rails runner cmd/recompute_judge_rollups.rb
+# will recompute judge stat's all of the flights for a year
 
 def do_year(year)
-  puts "Compute judge rollups for #{year}"
-  IAC::JudgeRollups.compute_jy_results(year)
+  puts "Queue jobs to compute all judging statistics for #{year}"
+  contests = Contest.where(['year(start) = ?', year])
+  contests.each do |contest|
+    puts "Queue Judge Flight Metrics Job for #{contest}"
+    Delayed::Job.enqueue Jobs::ComputeJudgeFlightMetricsJob.new(contest)
+  end
 end
 
 year = ARGV.empty? ? 0 : ARGV[0].to_i
 if (0 < year)
   do_year(year)
 else
-  contests = Contest.select("distinct year(start) as year")
-  contests.each { |contest| do_year(contest.year) }
+  puts "Specify the year"
 end
 
