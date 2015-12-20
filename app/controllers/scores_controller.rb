358a142f3a2af9c305d32a44e3200f8378e6e2e1
Douglas Lovell <doug@wbreeze.com>
2015-12-20 13:19:06 -0700

    separate the categories in the pilot contest flights listing

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -4,10 +4,10 @@ class ScoresController < ApplicationController
   def show
     @contest = Contest.find(params[:id])
     @pilot = Member.find(params[:pilot_id])
-    @pilot_flights = PilotFlight.joins(:flight).where(
+    @pilot_flights = PilotFlight.joins(:flight => :category).where(
        {:pilot_flights => {pilot_id: @pilot},
         :flights => {contest_id: @contest}}
-      ).order("flights.sequence")
+      ).order("categories.sequence, flights.sequence")
   end
 
 end
