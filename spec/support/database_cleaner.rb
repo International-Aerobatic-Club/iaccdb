2166dea643eaca1d2ac93252aa569001971b2241
Douglas Lovell <doug@wbreeze.com>
2016-05-09 22:29:47 -0600

    correct use of database cleaner, esp. for selenium-webdriver

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -2,16 +2,23 @@
 # spec/support/database_cleaner.rb
 RSpec.configure do |config|
   config.before(:suite) do
-    DatabaseCleaner.clean_with :truncation
-    ActiveRecord::Tasks::DatabaseTasks.load_seed
+    DatabaseCleaner.clean_with :truncation, { except: %w[ categories ] }
+  end
+
+  config.before(:each) do
     DatabaseCleaner.strategy = :transaction
   end
 
-  config.before(:context) do
+  # selenium driver as separate process breaks transaction strategy
+  config.before(:each, viz: true) do
+    DatabaseCleaner.strategy = :truncation, { except: %w[ categories ] }
+  end
+
+  config.before(:each) do
     DatabaseCleaner.start
   end
 
-  config.after(:context) do
+  config.after(:each) do
     DatabaseCleaner.clean
   end
 end
