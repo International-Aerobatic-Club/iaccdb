e81dafd4ae9dccaeac0c37ba0614ab1d1b52748b
Douglas Lovell <doug@wbreeze.com>
2016-05-09 21:10:00 -0600

    add capacity for selenium capybara driver on viz

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -6,3 +6,27 @@ Capybara.configure do |config|
   config.javascript_driver = :webkit
   #config.default_wait_time = 15
 end
+
+Capybara::Webkit.configure do |config|
+  config.block_unknown_urls
+end
+
+RSpec.configure do |config|
+  # when viz: true is on the scenario, e.g.
+  #   RSpec.describe "interface property", :type => :feature, viz: true do ...
+  #   or
+  #   scenario 'make it so', viz: true do ...
+  # we force the selenium driver.
+  # It will override the javascript driver as well, in other words
+  # "viz: true" trumps "js:true"
+  config.around :example, viz: true do |ex|
+    hold_driver = Capybara.current_driver
+    hold_javascript_driver = Capybara.javascript_driver
+    Capybara.current_driver = :selenium
+    Capybara.javascript_driver = :selenium
+    ex.run
+    Capybara.current_driver = hold_driver
+    Capybara.javascript_driver = hold_javascript_driver
+  end
+end
+
