c4a97407866eb4249f36eb5b2fae805fd982c37f
Douglas Lovell <doug@wbreeze.com>
2016-10-05 22:18:07 -0600

    fix typo - missing character

--- cmd/acro/results_list.rb
+++ cmd/acro/results_list.rb
@@ -6,7 +6,7 @@ if (control_file)
   rl = ACRO::ResultsList.new(control_file)
   rl.populate_with_results_files
   rl.write_to_file
-  puts "Edit #{rl.file_name} with database flight idenifiers for the flights"
+  puts "Edit #{rl.file_name} with database flight identifiers for the flights"
 else
   puts 'Supply the name of the contest information file'
 end
