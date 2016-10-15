134cfdf093040c46a70143dc0f621b31941d1fa5
Douglas Lovell <doug@wbreeze.com>
2016-10-15 11:25:30 -0600

    over-engineer what used to be a hash

---
+++ app/services/member_merge/role_flight.rb
@@ -0,0 +1,58 @@
+module MemberMerge
+class RoleFlight
+  attr_reader :role, :flight, :contest
+
+  def initialize(role, flight)
+    unless roles.include?(role)
+      raise ArgumentError.new "Role #{role} must be one of #{roles.inspect}"
+    end
+    unless flight
+      raise ArgumentError.new 'Flight must not be nil'
+    end
+    unless flight.id
+      raise ArgumentError.new 'Flight must have a database record id'
+    end
+    @role = role
+    @flight = flight
+    @contest = contest
+  end
+
+  def hash
+    flight.id << 3 + ROLES.index(role)
+  end
+
+  def eql?(o)
+    o.instance_of?(self.class) && (o == self || o.hash == hash)
+  end
+
+  def self.roles
+    ROLES
+  end
+
+  def roles
+    self.class.roles
+  end
+
+  def self.role_name(role)
+    ROLE_NAMES[role] || "#{role.to_s} not recognized"
+  end
+
+  def role_name
+    self.class.role_name(role)
+  end
+
+  #######
+  private
+  #######
+
+  ROLES = [:competitor, :chief_judge, :assist_chief_judge, :line_judge, :assist_line_judge]
+  ROLE_NAMES = {
+    :competitor => 'Competitor',
+    :chief_judge => 'Chief Judge',
+    :assist_chief_judge => 'Chief Judge Assistant',
+    :line_judge => 'Line Judge',
+    :assist_line_judge => 'Line Judge Assistant'
+  }
+
+end
+end
---
+++ spec/services/member_merge/role_flight_spec.rb
@@ -0,0 +1,59 @@
+module MemberMerge
+describe RoleFlight do
+  before :context do
+    @flight_record = create(:flight)
+  end
+  it 'raises argument error given invalid role' do
+    expect{ RoleFlight.new(:fake, @flight_record) }.to raise_error(
+      ArgumentError)
+  end
+  it 'raises argument error given nil flight' do
+    expect{ RoleFlight.new(:competitor, nil) }.to raise_error(
+      ArgumentError)
+  end
+  it 'raises argument error given unsaved flight' do
+    expect{ RoleFlight.new(:competitor, build(:flight)) }.to raise_error(
+      ArgumentError)
+  end
+  it 'matches instances with same role and flight' do
+    rf1 = RoleFlight.new(:competitor, @flight_record)
+    rf2 = RoleFlight.new(:competitor, @flight_record)
+    expect(rf1.eql? rf2).to be true
+  end
+  it 'does not match instances with same role, different flight' do
+    rf1 = RoleFlight.new(:competitor, @flight_record)
+    rf2 = RoleFlight.new(:competitor, create(:flight))
+    expect(rf1.eql? rf2).to be false
+  end
+  it 'does not match instances with different role, same flight' do
+    rf1 = RoleFlight.new(:competitor, @flight_record)
+    rf2 = RoleFlight.new(:line_judge, @flight_record)
+    expect(rf1.eql? rf2).to be false
+  end
+  it 'does not match instances with different role, different flight' do
+    rf1 = RoleFlight.new(:competitor, @flight_record)
+    rf2 = RoleFlight.new(:line_judge, create(:flight))
+    expect(rf1.eql? rf2).to be false
+  end
+  it 'class provides list of valid roles' do
+    roles = RoleFlight.roles
+    expect(roles).to include(:line_judge)
+    expect(roles).to include(:competitor)
+  end
+  it 'instance provides list of valid roles' do
+    roles = RoleFlight.new(:line_judge, @flight_record).roles
+    expect(roles).to include(:assist_chief_judge)
+    expect(roles).to include(:assist_line_judge)
+  end
+  it 'class translates role for display' do
+    expect(RoleFlight.role_name(:line_judge)).to eq 'Line Judge'
+    expect(RoleFlight.role_name(:competitor)).to eq 'Competitor'
+  end
+  it 'instance translates role for display' do
+    expect(RoleFlight.new(:chief_judge, @flight_record).role_name).to eq(
+      'Chief Judge')
+    expect(RoleFlight.new(:assist_chief_judge, @flight_record).role_name).to eq(
+      'Chief Judge Assistant')
+  end
+end
+end
