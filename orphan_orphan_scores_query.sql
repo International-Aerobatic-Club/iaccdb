e511569e740cef98e7718195373229f356bc8922
Douglas Lovell <doug@wbreeze.com>
2016-10-25 21:22:48 -0600

    finish renaming orphan scores query

--- orphan_orphan_scores_query.sql
+++
@@ -1,1 +0,0 @@
-select * from contests where id in (select contest_id from flights where id in (select distinct id from flights where id in (select flight_id from pilot_flights where id in (select pilot_flight_id from scores where not exists (select * from judges where scores.judge_id = id)))));
