8e95fda3cc3cb36de418600ca2317cb4657671ed
Douglas Lovell <doug@wbreeze.com>
2016-11-05 20:53:19 -0600

    correct the id pulled for pf json test

--- spec/controllers/pilot_flights_controller_spec.rb
+++ spec/controllers/pilot_flights_controller_spec.rb
@@ -8,10 +8,10 @@ describe PilotFlightsController, :type => :controller do
     data = JSON.parse(response.body)
     d_pf = data['pilot_flight_data']
     expect(d_pf).to_not be nil
-    expect(d_pf['id']).to eq @jf_result.id
+    expect(d_pf['id']).to eq @pilot_flight.id
   end
   it 'responds with pilot information' do
-    get :show, id: @jf_result.id, :format => :json
+    get :show, id: @pilot_flight.id, :format => :json
     data = JSON.parse(response.body)
     d_pf = data['pilot_flight_data']
     pilot = @pilot_flight.pilot
@@ -22,7 +22,7 @@ describe PilotFlightsController, :type => :controller do
     expect(d_p['iac_id']).to eq pilot.iac_id
   end
   it 'responds with pilot flight summary data' do
-    get :show, id: @jf_result.id, :format => :json
+    get :show, id: @pilot_flight.id, :format => :json
     data = JSON.parse(response.body)
     d_pf = data['pilot_flight_data']
     expect(d_pf['chapter']).to eq @pilot_flight.chapter
@@ -30,7 +30,7 @@ describe PilotFlightsController, :type => :controller do
     expect(d_pf['penalty_total']).to eq @pilot_flight.penalty_total
   end
   it 'responds with the airplane flown' do
-    get :show, id: @jf_result.id, :format => :json
+    get :show, id: @pilot_flight.id, :format => :json
     data = JSON.parse(response.body)
     d_pf = data['pilot_flight_data']
     d_a = d_pf['airplane']
@@ -40,7 +40,7 @@ describe PilotFlightsController, :type => :controller do
     expect(d_a['reg']).to eq @pilot_flight.airplane.reg
   end
   it 'responds with the flight information' do
-    get :show, id: @jf_result.id, :format => :json
+    get :show, id: @pilot_flight.id, :format => :json
     data = JSON.parse(response.body)
     d_pf = data['pilot_flight_data']
     d_f = d_pf['flight']
@@ -52,7 +52,7 @@ describe PilotFlightsController, :type => :controller do
     expect(d_f['category']).to eq flight.category.name
   end
   it 'responds with judge flight grades for pilot' do
-    get :show, id: @jf_result.id, :format => :json
+    get :show, id: @pilot_flight.id, :format => :json
     data = JSON.parse(response.body)
     d_pf = data['pilot_flight_data']
     d_gs = d_pf['grades']
@@ -77,7 +77,7 @@ describe PilotFlightsController, :type => :controller do
     expect(d_a['name']).to eq judge_pair.assist.name
   end
   it 'responds with flight sequence information' do
-    get :show, id: @jf_result.id, :format => :json
+    get :show, id: @pilot_flight.id, :format => :json
     data = JSON.parse(response.body)
     d_pf = data['pilot_flight_data']
     d_s = d_pf['sequence']
