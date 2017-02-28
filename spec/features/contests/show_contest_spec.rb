cc807829b16132a2157f205753218475eac15915
Douglas Lovell <doug@wbreeze.com>
2017-02-27 23:06:45 -0300

    spec and implement conditions when chiefs not shown

--- spec/features/contests/show_contest_spec.rb
+++ spec/features/contests/show_contest_spec.rb
@@ -15,46 +15,49 @@ describe 'show contest' do
     end
     [first, second, third]
   end
-  before :context do
-    @contest = create(:contest)
-    spn = Category.find_by(aircat: 'P', category: 'sportsman')
-    imdt = Category.find_by(aircat: 'P', category: 'intermediate')
-    @cjs = create(:member)
-    @cji = create(:member)
-    @spn_flights = build_contest_category_flights(@contest, spn, @cjs)
-    @imdt_flights = build_contest_category_flights(@contest, imdt, @cji)
-    computer = ContestComputer.new(@contest)
-    computer.compute_results
-  end
-  it 'shows chief judge rollup for contest' do
-    visit contest_path(@contest)
-    within 'div#content p.contest_chief' do
-      expect(page).to have_content('Chief Judge(s)')
-      expect(page).to have_content(@cjs.name)
-      expect(page).to have_content(@cji.name)
+  context 'multiple chiefs' do
+    before :context do
+      @contest = create(:contest)
+      spn = Category.find_by(aircat: 'P', category: 'sportsman')
+      imdt = Category.find_by(aircat: 'P', category: 'intermediate')
+      @cjs = create(:member)
+      @cji = create(:member)
+      @spn_flights = build_contest_category_flights(@contest, spn, @cjs)
+      @imdt_flights = build_contest_category_flights(@contest, imdt, @cji)
+      computer = ContestComputer.new(@contest)
+      computer.compute_results
     end
-  end
-  it 'shows chief judge rollup for category' do
-    cj2 = create(:member)
-    fspn = @spn_flights.first
-    fspn.chief = cj2
-    fspn.save!
-    visit contest_path(@contest)
-    spnh = find(:xpath, "//div[@id='content']/h3[text()='#{fspn.category.name}']")
-    ptbl = spnh.first(:xpath, "following-sibling::table[@class='pilot_results']")
-    expect(ptbl).to_not be nil
-    pfcj = ptbl.first(:xpath, "following-sibling::p[@class='category-chief']")
-    expect(pfcj).to_not be nil
-    expect(pfcj).to have_content('Chief Judge(s)')
-    expect(pfcj).to have_content(@cjs.name)
-    expect(pfcj).to have_content(cj2.name)
+    it 'shows chief judge rollup for contest' do
+      visit contest_path(@contest)
+      within 'div#content p.contest_chief' do
+        expect(page).to have_content('Chief Judge(s)')
+        expect(page).to have_content(@cjs.name)
+        expect(page).to have_content(@cji.name)
+      end
+    end
+    it 'shows chief judge rollup for category' do
+      cj2 = create(:member)
+      fspn = @spn_flights.first
+      fspn.chief = cj2
+      fspn.save!
+      visit contest_path(@contest)
+      spnh = find(:xpath, "//div[@id='content']/h3[text()='#{fspn.category.name}']")
+      ptbl = spnh.first(:xpath, "following-sibling::table[@class='pilot_results']")
+      expect(ptbl).to_not be nil
+      pfcj = ptbl.first(:xpath, "following-sibling::p[@class='category-chief']")
+      expect(pfcj).to_not be nil
+      expect(pfcj).to have_content('Chief Judge(s)')
+      expect(pfcj).to have_content(@cjs.name)
+      expect(pfcj).to have_content(cj2.name)
+    end
   end
   context 'no chiefs' do
     before :context do
-      [@spn_flights, @imdt_flights].flatten.each do |f|
-        f.chief = nil
-        f.save!
-      end
+      @contest = create(:contest)
+      spn = Category.find_by(aircat: 'P', category: 'sportsman')
+      build_contest_category_flights(@contest, spn, nil)
+      computer = ContestComputer.new(@contest)
+      computer.compute_results
     end
     it 'does not show contest chiefs stem' do
       visit contest_path(@contest)
@@ -64,6 +67,18 @@ describe 'show contest' do
       visit contest_path(@contest)
       expect(page).to_not have_css('p.category-chief');
     end
+  end
+  it 'does not show chief on categories when same for all' do
+    @contest = create(:contest)
+    spn = Category.find_by(aircat: 'P', category: 'sportsman')
+    imdt = Category.find_by(aircat: 'P', category: 'intermediate')
+    @cj = create(:member)
+    @spn_flights = build_contest_category_flights(@contest, spn, @cj)
+    @imdt_flights = build_contest_category_flights(@contest, imdt, @cj)
+    computer = ContestComputer.new(@contest)
+    computer.compute_results
+    visit contest_path(@contest)
+    expect(page).to_not have_css('p.category-chief');
   end
 end
 
