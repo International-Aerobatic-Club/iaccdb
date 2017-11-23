7a150bb560b64f2d96b60382cb64cd4a955738ce
Douglas Lovell <doug@wbreeze.com>
2017-11-23 19:47:34 -0300

    Correctly construct ProcessJasperJob in spec

--- spec/jobs/process_jasper_spec.rb
+++ spec/jobs/process_jasper_spec.rb
@@ -11,7 +11,7 @@ module Jobs
       @data_post.save!
     end
     it 'processes the data post without error' do
-      job = ProcessJasperJob.new(@data_post)
+      job = ProcessJasperJob.new(@data_post.id)
       begin
         job.perform
       rescue => e
@@ -22,14 +22,14 @@ module Jobs
       expect(@data_post.error_description).to be nil
     end
     it 'processes the data post into a contest entry' do
-      job = ProcessJasperJob.new(@data_post)
+      job = ProcessJasperJob.new(@data_post.id)
       job.perform
       @contest.reload
       expect(@contest.name).to eq 'Test Contest US Candian Challenge'
       expect(@contest.region).to eq 'NorthEast'
     end
     it 'processes the contest data' do
-      job = ProcessJasperJob.new(@data_post)
+      job = ProcessJasperJob.new(@data_post.id)
       job.perform
       @contest.reload
       expect(@contest.name).to eq 'Test Contest US Candian Challenge'
@@ -38,7 +38,7 @@ module Jobs
     end
     it 'runs the compute workflow' do
       expect(JyResult.all.count).to eq 0
-      Delayed::Job.enqueue ProcessJasperJob.new(@data_post)
+      Delayed::Job.enqueue ProcessJasperJob.new(@data_post.id)
       work_result = Delayed::Worker.new.work_off
       expect(work_result[1]).to eq 0
       expect(JyResult.all.count).to be > 0
