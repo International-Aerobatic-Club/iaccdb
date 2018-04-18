0daf58c23278df7955dd10e695132c3fcd41051d
Douglas Lovell <doug@wbreeze.com>
2018-04-18 16:36:29 -0300

    Create contests directory for contest integration tests

--- test/integration/contests/show_test.rb
+++ test/integration/contests/show_test.rb
@@ -1,11 +1,11 @@
 require 'test_helper'
 
-class ContestsTest < ActionDispatch::IntegrationTest
+class ContestsShowTest < ActionDispatch::IntegrationTest
   test "contest with no results" do
     contest = create(:contest)
     visit contest_path(contest.id)
     within('div#content') do
-      assert(page.body.include?('This contest has not posted any results.'))
+      assert(page.has_text?('This contest has not posted any results.'))
     end
   end
 end
