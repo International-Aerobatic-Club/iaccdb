3d4e295c0a9f2aea5831dd23d72f3ab1d52cd836
Douglas Lovell <doug@wbreeze.com>
2018-04-21 13:37:44 -0300

    Migrate airplanes to airplane.make_model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180420174501) do
+ActiveRecord::Schema.define(version: 20180421163508) do
 
   create_table "airplanes", force: :cascade do |t|
     t.string   "make",          limit: 255
