3133b93d8c58a0100a4e9f5cfd8ae0a89f76f803
Douglas Lovell <doug@wbreeze.com>
2018-04-26 13:13:51 -0300

    Replace positional with keyword arguments in controller minitest

--- test/controllers/authorization_test.rb
+++ test/controllers/authorization_test.rb
@@ -2,14 +2,14 @@ require 'test_helper'
 
 class AuthorizationTest < ActionDispatch::IntegrationTest
   test 'authorized admin gets admin role' do
-    get '/admin/contests', nil, http_auth_login(:admin)
+    get '/admin/contests', headers: http_auth_login(:admin)
     assert_response(:success)
     assert_equal(:admin, @controller.current_role)
   end
 
   test 'authorized contest admin gets contest role' do
     contest = create(:contest)
-    delete contest_path(contest), nil, http_auth_login(:contest_admin)
+    delete contest_path(contest), headers: http_auth_login(:contest_admin)
     assert_response(:success)
     assert_equal(:contest_admin, @controller.current_role)
   end
