a8a42f047ce608e901e10791b18f98d9663a488f
Douglas Lovell <doug@wbreeze.com>
2018-05-02 23:25:51 -0300

    [issue61]Return bad request when contest_xml empty

--- app/controllers/admin/jasper_controller.rb
+++ app/controllers/admin/jasper_controller.rb
@@ -4,39 +4,41 @@ class Admin::JasperController < ApplicationController
 
 skip_before_action :verify_authenticity_token
 
+class EmptyDataException < Exception
+end
+
 def results
   @exception = nil
   status = 200
+  post_record = DataPost.create
   begin
     xml_data = params[:contest_xml]
-    post_record = DataPost.create
     if xml_data && xml_data.is_a?(String)
+      raise EmptyDataException if xml_data.empty?
       logger.info "JasperController: parse xml param"
       post_record.store(xml_data)
     elsif xml_data
       logger.info "JasperController: read xml file"
       post_record.store(xml_data.read)
     else
-      @exception = "invalid post"
-      status = 400
-      post_record.has_error = true
-      post_record.error_description = @exception
-      post_record.store('<ContestResults/>')
+      raise EmptyDataException
     end
-    post_record.save!
-    if (@exception == nil)
-      parser = LibXML::XML::Parser.string(post_record.data)
-      jasper = Jasper::JasperParse.new
-      jasper.do_parse(parser)
-      @contest_id = jasper.contest_id
-      if (@contest_id == nil || @contest_id <= 0)
-        j2d = Jasper::JasperToDB.new
-        contest = Contest.create! j2d.extract_contest_params_hash(jasper)
-        @contest_id = contest.id
-      end
-      post_record.contest_id = @contest_id
-      post_record.save!
+    parser = LibXML::XML::Parser.string(post_record.data)
+    jasper = Jasper::JasperParse.new
+    jasper.do_parse(parser)
+    @contest_id = jasper.contest_id
+    if (@contest_id == nil || @contest_id <= 0)
+      j2d = Jasper::JasperToDB.new
+      contest = Contest.create! j2d.extract_contest_params_hash(jasper)
+      @contest_id = contest.id
     end
+    post_record.contest_id = @contest_id
+  rescue EmptyDataException => ex
+    @exception = "no contest data provided"
+    post_record.store('<ContestResults/>')
+    status = 400
+    post_record.has_error = true
+    post_record.error_description = @exception
   rescue Exception => ex
     @exception = ex.message
     status = 500
@@ -44,10 +46,11 @@ def results
     logger.error ex.backtrace.join('\n')
     post_record.has_error = true
     post_record.error_description = @exception
+  ensure
     post_record.save!
   end
   self.formats = [:xml]
-  if (@exception == nil)
+  if (status == 200)
     Delayed::Job.enqueue Jobs::ProcessJasperJob.new(post_record.id)
     render :results
   else
