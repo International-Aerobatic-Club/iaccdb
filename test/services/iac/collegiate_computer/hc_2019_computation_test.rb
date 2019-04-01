cc88216f84e34f9cf02bd59a2dd549d6d0c2eed1
Douglas Lovell <doug@wbreeze.com>
2019-04-01 15:21:11 -0300

    Test for 2019 collegiate includes solo in category

--- test/services/iac/collegiate_computer/hc_2019_computation_test.rb
+++ test/services/iac/collegiate_computer/hc_2019_computation_test.rb
@@ -7,16 +7,16 @@ module IAC
 
     setup do
       setup_collegiate_participation(2019)
-      @mills_hcr = PcResult.create(pilot: @pilot_mills,
+      @mills_hcr = PcResult.create!(pilot: @pilot_mills,
         category: @spn,
         contest: @c_michg,
-        hors_concours: true,
         category_value: 3500.00, total_possible: 4080)
     end
 
     test 'omits HC lower category computing team' do
+      @mills_hcr.hc_higher_category.save!
       @team.pc_results << @mills_hcr
-      @team.save
+      @team.save!
       computer = CollegiateComputer.new(@year)
       computer.recompute_team
       @team.reload
@@ -24,12 +24,34 @@ module IAC
     end
 
     test 'omits HC lower category computing individual' do
+      @mills_hcr.hc_higher_category.save!
       @mills.pc_results << @mills_hcr
-      @mills.save
+      @mills.save!
       computer = CollegiateIndividualComputer.new(@year)
       computer.recompute
       @mills.reload
       assert_equal(9971.66, @mills.points)
+      assert_equal(12240, @mills.points_possible)
+    end
+
+    test 'includes HC no competition computing team' do
+      @mills_hcr.hc_no_competition.save!
+      @team.pc_results << @mills_hcr
+      @team.save!
+      computer = CollegiateComputer.new(@year)
+      computer.recompute_team
+      @team.reload
+      assert_equal(6453.51, @team.points)
+    end
+
+    test 'includes HC no competition computing individual' do
+      @mills_hcr.hc_no_competition.save!
+      @mills.pc_results << @mills_hcr
+      @mills.save!
+      computer = CollegiateIndividualComputer.new(@year)
+      computer.recompute
+      @mills.reload
+      assert_equal(10160.56, @mills.points)
       assert_equal(12240, @mills.points_possible)
     end
   end
