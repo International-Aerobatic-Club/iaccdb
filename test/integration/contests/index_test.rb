1d5b3d6c20f58c8adb0112d0f80aeb4ed1292078
Douglas Lovell <doug@wbreeze.com>
2019-08-28 13:52:56 -0300

    Migrate contest#index test to NokoGiri"

--- test/integration/contests/index_test.rb
+++ test/integration/contests/index_test.rb
@@ -15,27 +15,32 @@ class ContestsIndexTest < ActionDispatch::IntegrationTest
   end
 
   def contest_item_link_selector(contest)
-    "//li//a[@href='#{contest_path(contest)}']"
+    "li//a[@href='#{contest_path(contest)}']"
   end
 
   def contest_item_link(contest)
-    page.find(:xpath, contest_item_link_selector(contest))
+    link = nil
+    assert_select(contest_item_link_selector(contest)) do |contest|
+      link = contest.first
+    end
+    assert(link)
+    link
   end
 
   def future_contest_item(item_link)
-    item_link.has_xpath?('ancestor::ul[contains(@class, "future-contests")]')
+    item_link.xpath('ancestor::ul[contains(@class, "future-contests")]')
   end
 
   test 'future contests separated into future' do
-    visit contests_path
-    within('div#content') do
-      assert(page.has_xpath?('//h3[contains(text(),"Future contests")]'))
+    get contests_path
+    assert_select('div#content') do |content|
+      content = content.first
+      assert(content.xpath('//h3[contains(text(),"Future contests")]'))
       @contests.each do |contest|
         item_link = contest_item_link(contest)
-        assert(item_link)
         if contest.start <= @refdate
           # contests started or completed
-          refute(future_contest_item(item_link))
+          assert_empty(future_contest_item(item_link))
         else
           # future contests
           assert(future_contest_item(item_link))
@@ -46,41 +51,45 @@ class ContestsIndexTest < ActionDispatch::IntegrationTest
 
   test 'contests shown for current year' do
     future_season_contest = create :contest, year: @refdate.year + 1
-    visit contests_path
-    refute(page.has_xpath?(contest_item_link_selector(future_season_contest)))
+    get contests_path
+    assert_select('div#content') do |content|
+      content = content.first
+      assert_empty(content.xpath(
+        contest_item_link_selector(future_season_contest)))
+    end
     assert(contest_item_link(@contests.first))
   end
 
   test 'post contests show post date when posted' do
     contest_with_flight = create(:contest, start: @refdate - 1.week)
     flight = create :flight, contest: contest_with_flight
-    visit contests_path
+    get contests_path
     item_link = contest_item_link(contest_with_flight)
-    refute(future_contest_item(item_link))
-    assert(item_link.has_xpath?(
+    assert_empty(future_contest_item(item_link))
+    refute_empty(item_link.xpath(
       "following-sibling::text()[contains(., 'computed #{flight.created_at}')]"
     ))
   end
 
   test 'post contests show not yet posted' do
     contest = create(:contest, start: @refdate - 1.week)
-    visit contests_path
+    get contests_path
     item_link = contest_item_link(contest)
-    refute(future_contest_item(item_link))
-    assert(item_link.has_xpath?(
+    assert_empty(future_contest_item(item_link))
+    refute_empty(item_link.xpath(
       'following-sibling::*[contains(@class, "missing-results")]'
     ))
   end
 
   test 'future contests do not show not posted' do
     contest = create(:contest, start: @refdate + 1.week)
-    visit contests_path
+    get contests_path
     item_link = contest_item_link(contest)
     assert(future_contest_item(item_link))
-    refute(item_link.has_xpath?(
+    assert_empty(item_link.xpath(
       'following-sibling::*[contains(@class, "missing-results")]'
     ))
-    refute(item_link.has_xpath?(
+    assert_empty(item_link.xpath(
       'following-sibling::text()[contains(., "computed")]'
     ))
   end
