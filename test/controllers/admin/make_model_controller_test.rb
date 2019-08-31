1005596f944e54625d6e15d58ea5f751fefe7ecf
Douglas Lovell <doug@wbreeze.com>
2019-08-31 20:23:39 -0300

    Add admin form for viewing and merging make_models

--- test/controllers/admin/make_model_controller_test.rb
+++ test/controllers/admin/make_model_controller_test.rb
@@ -18,6 +18,20 @@ class Admin::MakeModelControllerTest < ActionDispatch::IntegrationTest
   test 'authorized can view index' do
     get admin_make_models_path, headers: http_auth_login(:curator)
     assert_response :success
+    assert_select("form[action=\"#{admin_make_models_merge_preview_path}\"]")
+    assert_select('form table tbody') do |tbody|
+      tbody = tbody.first
+      @models.each do |m|
+        input = tbody.xpath(
+          "./tr/td/input[@type=\"checkbox\" and @name=\"selected[#{m.id}]\"]"
+        )
+        assert_equal(1, input.length, "Have checkbox for make_model #{m.id}")
+        assert_select('tr td', m.make)
+        assert_select('tr td', m.model)
+        assert_select("tr td a[href=\"#{edit_admin_make_model_path(m)}\"]")
+        assert_select('tr td input[@type="submit"]')
+      end
+    end
   end
 
   test 'authorized can show' do
