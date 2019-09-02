a4be34ae0726fa8eeda61967f00a20912eb24648
Douglas Lovell <doug@wbreeze.com>
2019-09-02 12:55:51 -0300

    Add make model service instantiation and airplanes list

--- app/services/make_model_service.rb
+++ app/services/make_model_service.rb
@@ -1,4 +1,11 @@
 class MakeModelService
+  attr_reader :target, :source
+
+  def initialize(target, source)
+    @target = target
+    @source = source
+  end
+
   def self.associate_airplane_make_models
     Airplane.all.each do |a|
       if (a.make or a.model)
@@ -7,5 +14,13 @@ class MakeModelService
         a.save!
       end
     end
+  end
+
+  def target_airplanes
+    target.airplanes.to_a
+  end
+
+  def source_airplanes
+    source.airplanes.to_a
   end
 end
--- test/services/make_model/merge_test.rb
+++ test/services/make_model/merge_test.rb
@@ -6,11 +6,32 @@ class MakeModel::MergeTest < ActiveSupport::TestCase
     MakeModel.all.each do |mm|
       create_list(:airplane, Random.rand(7) + 2, make_model: mm)
     end
+    mms = MakeModel.limit(2).all
+    @svc = MakeModelService.new(mms[1], mms[0])
   end
 
   test 'make_models have multiple airplanes' do
     MakeModel.all.each do |mm|
       assert_operator(1, :<, mm.airplanes.count)
     end
+  end
+
+  test 'merge service initialization' do
+    mms = MakeModel.limit(2).all
+    assert_equal(2, mms.count)
+    target = mms[1];
+    source = mms[0];
+    svc = MakeModelService.new(target, source)
+    assert_equal(target.id, svc.target.id)
+    assert_equal(source.id, svc.source.id)
+  end
+
+  test 'merge service airplanes lists' do
+    target_airplanes = @svc.target.airplanes.to_a
+    source_airplanes = @svc.source.airplanes.to_a
+    assert_equal(target_airplanes.collect(&:id).sort,
+      @svc.target_airplanes.collect(&:id).sort)
+    assert_equal(source_airplanes.collect(&:id).sort,
+      @svc.source_airplanes.collect(&:id).sort)
   end
 end
