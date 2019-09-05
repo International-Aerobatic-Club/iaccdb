0d4fb09a902931d3cbaf33e3582c476f0dcc006b
Douglas Lovell <doug@wbreeze.com>
2019-09-05 16:11:04 -0300

    Ensure make_model data contains mix of curated and not

--- test/shared/make_models_data.rb
+++ test/shared/make_models_data.rb
@@ -2,7 +2,8 @@ require "test_helper"
 
 module MakeModelsData
   def setup_make_models_data
-    mmods = build_list :make_model, Random::rand(4) + 2
+    mmods = build_list :make_model, Random::rand(5) + 3, curated: false
+    mmods += build_list :make_model, Random::rand(5) + 3, curated: true
     mmods.collect(&:make).uniq.inject(Hash.new) do |memo, make|
       memo[make] = create_list(:make_model, Random::rand(5) + 1, make: make)
       memo
