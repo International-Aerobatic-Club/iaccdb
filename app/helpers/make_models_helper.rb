f932847f454c44fdecdc612a2587df8ca0c9c9d3
Douglas Lovell <doug@wbreeze.com>
2019-09-08 19:20:29 -0300

    Display fuller make-model info in preview and regular index

--- app/helpers/make_models_helper.rb
+++ app/helpers/make_models_helper.rb
@@ -6,4 +6,40 @@ module MakeModelsHelper
   def model_or_unspecified(model)
     model.blank? ? "{no model specified}" : model
   end
+
+  def make_model_wings(mm)
+    number = (mm.wings == nil || mm.wings < 1 || 2 < mm.wings) ? 0 : mm.wings
+    [nil, 'monoplane', 'biplane'][number]
+  end
+
+  def make_model_horsepower(mm)
+    (mm.horsepower && 0 < mm.horsepower) ? "#{mm.horsepower}hp" : nil
+  end
+
+  def make_model_seats(mm)
+    (mm.seats && 0 < mm.seats) ? pluralize(mm.seats, "seat") : nil
+  end
+
+  def make_model_curated(mm)
+    mm.curated ? '👍 ' : ''
+  end
+
+  def make_model_description(mm)
+    [
+      make_model_curated(mm) + make_or_unspecified(mm.make),
+      model_or_unspecified(mm.model),
+      make_model_horsepower(mm),
+      make_model_wings(mm),
+      make_model_seats(mm)
+    ].compact.join(', ')
+  end
+
+  def model_description(mm)
+    [
+      make_model_curated(mm) + model_or_unspecified(mm.model),
+      make_model_horsepower(mm),
+      make_model_wings(mm),
+      make_model_seats(mm)
+    ].compact.join(', ')
+  end
 end
