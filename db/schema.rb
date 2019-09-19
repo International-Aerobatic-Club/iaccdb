95ca10c5941ee8b00c7d811a4ace11247bba3bf3
Douglas Lovell <doug@wbreeze.com>
2019-09-19 12:55:54 -0300

    Add synthetic category table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190905130441) do
+ActiveRecord::Schema.define(version: 20190919151543) do
 
-  create_table "airplanes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "airplanes", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.string "reg"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20190905130441) do
     t.index ["id"], name: "index_airplanes_on_id"
   end
 
-  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "categories", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.integer "sequence", null: false
     t.string "category", limit: 16, null: false
     t.string "aircat", limit: 1, null: false
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20190905130441) do
     t.index ["id"], name: "index_categories_on_id"
   end
 
-  create_table "contests", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "contests", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.string "name", limit: 48
     t.string "city", limit: 24
     t.string "state", limit: 2
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 20190905130441) do
     t.index ["id"], name: "index_contests_on_id"
   end
 
-  create_table "data_posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "data_posts", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.integer "contest_id"
     t.boolean "is_integrated", default: false
     t.boolean "has_error", default: false
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 20190905130441) do
     t.index ["id"], name: "index_data_posts_on_id"
   end
 
-  create_table "delayed_jobs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "delayed_jobs", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.integer "priority", default: 0
     t.integer "attempts", default: 0
     t.text "handler", limit: 16777215
@@ -70,7 +70,7 @@ ActiveRecord::Schema.define(version: 20190905130441) do
     t.index ["priority", "run_at"], name: "delayed_jobs_priority"
   end
 
-  create_table "failures", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "failures", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.string "step", limit: 16
     t.integer "contest_id"
     t.integer "manny_id"
@@ -84,7 +84,7 @@ ActiveRecord::Schema.define(version: 20190905130441) do
     t.index ["manny_id"], name: "index_failures_on_manny_id"
   end
 
-  create_table "flights", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "flights", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.integer "contest_id", null: false
     t.string "name", limit: 16, null: false
     t.integer "sequence", null: false
@@ -100,7 +100,7 @@ ActiveRecord::Schema.define(version: 20190905130441) do
     t.index ["id"], name: "index_flights_on_id"
   end
 
-  create_table "jc_results", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "jc_results", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.integer "judge_id", null: false
     t.integer "pilot_count"
     t.decimal "sigma_ri_delta", precision: 11, scale: 5
@@ -127,7 +127,7 @@ ActiveRecord::Schema.define(version: 20190905130441) do
     t.index ["judge_id"], name: "index_jc_results_on_judge_id"
   end
 
-  create_table "jf_results", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "jf_results", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.integer "judge_id"
     t.integer "pilot_count"
     t.decimal "sigma_ri_delta", precision: 10, scale: 5
@@ -152,7 +152,7 @@ ActiveRecord::Schema.define(version: 20190905130441) do
     t.index ["judge_id"], name: "index_jf_results_on_judge_id"
   end
 
-  create_table "judges", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "judges", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.integer "judge_id"
     t.integer "assist_id"
     t.datetime "created_at"
@@ -162,7 +162,7 @@ ActiveRecord::Schema.define(version: 20190905130441) do
     t.index ["judge_id"], name: "index_judges_on_judge_id"
   end
 
-  create_table "jy_results", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "jy_results", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.integer "judge_id"
     t.integer "category_id"
     t.integer "year"
@@ -188,7 +188,7 @@ ActiveRecord::Schema.define(version: 20190905130441) do
     t.index ["judge_id"], name: "index_jy_results_on_judge_id"
   end
 
-  create_table "make_models", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "make_models", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.string "make", limit: 80
     t.string "model", limit: 80
     t.integer "empty_weight_lbs", limit: 2
@@ -203,7 +203,7 @@ ActiveRecord::Schema.define(version: 20190905130441) do
     t.index ["model"], name: "index_make_models_on_model"
   end
 
-  create_table "manny_synches", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "manny_synches", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.integer "contest_id"
     t.integer "manny_number"
     t.datetime "synch_date"
@@ -212,7 +212,7 @@ ActiveRecord::Schema.define(version: 20190905130441) do
     t.index ["contest_id"], name: "index_manny_synches_on_contest_id"
   end
 
-  create_table "members", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "members", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.integer "iac_id"
     t.string "given_name", limit: 40
     t.string "family_name", limit: 40
@@ -222,7 +222,7 @@ ActiveRecord::Schema.define(version: 20190905130441) do
     t.index ["id"], name: "index_members_on_id"
   end
 
-  create_table "pc_results", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "pc_results", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.integer "pilot_id", null: false
     t.decimal "category_value", precision: 8, scale: 2
     t.integer "category_rank"
@@ -240,7 +240,7 @@ ActiveRecord::Schema.define(version: 20190905130441) do
     t.index ["pilot_id"], name: "index_pc_results_on_pilot_id"
   end
 
-  create_table "pf_results", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "pf_results", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.integer "pilot_flight_id", null: false
     t.decimal "flight_value", precision: 7, scale: 2
     t.decimal "adj_flight_value", precision: 7, scale: 2
@@ -256,7 +256,7 @@ ActiveRecord::Schema.define(version: 20190905130441) do
     t.index ["pilot_flight_id"], name: "index_pf_results_on_pilot_flight_id"
   end
 
-  create_table "pfj_results", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "pfj_results", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.integer "pilot_flight_id", null: false
     t.integer "judge_id", null: false
     t.string "computed_values"
@@ -273,7 +273,7 @@ ActiveRecord::Schema.define(version: 20190905130441) do
     t.index ["pilot_flight_id"], name: "index_pfj_results_on_pilot_flight_id"
   end
 
-  create_table "pilot_flights", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "pilot_flights", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.integer "pilot_id"
     t.integer "flight_id"
     t.integer "sequence_id"
@@ -290,7 +290,7 @@ ActiveRecord::Schema.define(version: 20190905130441) do
     t.index ["sequence_id"], name: "index_pilot_flights_on_sequence_id"
   end
 
-  create_table "region_contests", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "region_contests", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.integer "pc_result_id"
     t.integer "regional_pilot_id"
     t.datetime "created_at"
@@ -300,7 +300,7 @@ ActiveRecord::Schema.define(version: 20190905130441) do
     t.index ["regional_pilot_id"], name: "index_region_contests_on_regional_pilot_id"
   end
 
-  create_table "regional_pilots", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "regional_pilots", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.integer "pilot_id"
     t.string "region", limit: 16, null: false
     t.integer "year"
@@ -315,7 +315,7 @@ ActiveRecord::Schema.define(version: 20190905130441) do
     t.index ["pilot_id"], name: "index_regional_pilots_on_pilot_id"
   end
 
-  create_table "result_accums", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "result_accums", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.integer "result_id"
     t.integer "pc_result_id"
     t.datetime "created_at", null: false
@@ -325,7 +325,7 @@ ActiveRecord::Schema.define(version: 20190905130441) do
     t.index ["result_id"], name: "index_result_accums_on_result_id"
   end
 
-  create_table "result_members", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "result_members", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.integer "member_id"
     t.integer "result_id"
     t.datetime "created_at", null: false
@@ -335,7 +335,7 @@ ActiveRecord::Schema.define(version: 20190905130441) do
     t.index ["result_id"], name: "index_result_members_on_result_id"
   end
 
-  create_table "results", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "results", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.string "type"
     t.integer "year"
     t.integer "category_id"
@@ -353,7 +353,7 @@ ActiveRecord::Schema.define(version: 20190905130441) do
     t.index ["pilot_id"], name: "index_results_on_pilot_id"
   end
 
-  create_table "scores", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "scores", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.integer "pilot_flight_id"
     t.integer "judge_id"
     t.string "values"
@@ -364,7 +364,7 @@ ActiveRecord::Schema.define(version: 20190905130441) do
     t.index ["pilot_flight_id"], name: "index_scores_on_pilot_flight_id"
   end
 
-  create_table "sequences", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "sequences", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.integer "figure_count"
     t.integer "total_k"
     t.integer "mod_3_total"
@@ -375,11 +375,19 @@ ActiveRecord::Schema.define(version: 20190905130441) do
     t.index ["id"], name: "index_sequences_on_id"
   end
 
-  create_table "writers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "name"
+  create_table "synthetic_categories", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+    t.integer "contest_id"
+    t.integer "regular_category_id"
+    t.text "regular_category_flights"
+    t.string "synthetic_category_name"
+    t.string "synthetic_category_description"
+    t.text "synthetic_category_flights"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["id"], name: "index_writers_on_id"
+    t.index ["contest_id"], name: "index_synthetic_categories_on_contest_id"
+    t.index ["regular_category_id"], name: "index_synthetic_categories_on_regular_category_id"
   end
 
+  add_foreign_key "synthetic_categories", "categories", column: "regular_category_id", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "synthetic_categories", "contests"
 end
