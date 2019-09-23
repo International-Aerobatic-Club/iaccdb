995b03201b64c0ee894ec4d91c53367ba51a108c
Douglas Lovell <doug@wbreeze.com>
2019-09-23 18:17:59 -0300

    Fixup pilot_flight scores display in scores controller

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -4,7 +4,7 @@ class ScoresController < ApplicationController
   def show
     @contest = Contest.find(params[:id])
     @pilot = Member.find(params[:pilot_id])
-    @pilot_flights = PilotFlight.joins(:flight => :category).where(
+    @pilot_flights = PilotFlight.joins(:flight => :categories).where(
        {:pilot_flights => {pilot_id: @pilot},
         :flights => {contest_id: @contest}}
       ).order("categories.sequence, flights.sequence")
