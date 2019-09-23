0a2b7915f8d8920d1ae0b8a4fb38da6d182cc6bc
Douglas Lovell <doug@wbreeze.com>
2019-09-22 21:37:39 -0300

    Synthesize categories for a contest

--- app/services/category_synthesis_service.rb
+++ app/services/category_synthesis_service.rb
@@ -1,25 +1,28 @@
 class CategorySynthesisService
-  def initialize(synthetic_category)
-    @sc = synthetic_category
-  end
-
-  def synthesize_category
-    reg_cat = @sc.regular_category
-    syn_cat = @sc.find_or_create
-    reg_flights = Flight.where(contest: @sc.contest, category: reg_cat)
-    @sc.synthetic_category_flights.each do |name|
+  def self.synthesize_category(synthetic_category)
+    sc = synthetic_category # shorthand
+    reg_cat = sc.regular_category
+    syn_cat = sc.find_or_create
+    reg_flights = Flight.where(contest: sc.contest, category: reg_cat)
+    sc.synthetic_category_flights.each do |name|
       reg_flight = Flight.find_by(
-        contest: @sc.contest, category: reg_cat, name: name
+        contest: sc.contest, category: reg_cat, name: name
       )
       reg_flight.extend(FlightM::CopyToCategory)
       reg_flight.copy_to_category(syn_cat)
     end
     non_reg_names =
-      @sc.synthetic_category_flights - @sc.regular_category_flights
+      sc.synthetic_category_flights - sc.regular_category_flights
     Flight.where(
-      contest: @sc.contest, category: reg_cat, name: non_reg_names
+      contest: sc.contest, category: reg_cat, name: non_reg_names
     ).each do |flight|
       flight.destroy!
+    end
+  end
+
+  def self.synthesize_categories(contest)
+    SyntheticCategory.where(contest: contest).each do |sc|
+      self.synthesize_category(sc)
     end
   end
 end
