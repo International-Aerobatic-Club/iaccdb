2915ba5c3e260fa06bf9a8beee2fb722c4a8823d
djmolny <djmolny@gmail.com>
2020-12-06 13:01:37 -0700

    Fixed a bug whereby we were only picking up the Flight object for the Known sequence for a single category per contest. Enhanced comments. Renamed 'flights' variable to 'known_flights' for clarity.

--- script/make-model-counts.rb
+++ script/make-model-counts.rb
@@ -1,27 +1,45 @@
-# Script to count the number times a given make/model pair competed starting in 2011
+# Script for counting the number of aircraft makes/models that have flown
+# in all contests starting in 2011.
+
 # DJM, 2020-12-06
 
+# Filler values
 UNKNOWN = 'Unknown'
 unk = MakeModel.new(make: UNKNOWN, model: UNKNOWN)
 
+# 
 def u(s)
   s.strip.blank? ? UNKNOWN : s
 end
 
+# Hash to hold make/model counts
 mmc = Hash.new
 
+
+
+# Get all contests from 2011 onwards
 contests = Contest.where('start >= ?', '2011-01-01')
 # puts "contests: #{contests.count}"
 
-flights = contests.map{ |c| c.flights.find_by_sequence(1) }.compact
-# puts "flights: #{flights.count}"
+# Get all Known flights for those contests
+# Note 1: There will be fewer Known flights than contest because the DB includes canceled contests,
+#         which is why we need to eliminate nil values using 'compact'.
+# Note 2: Known flights have a Flight.sequence value of 1.
 
-pilot_flights = flights.map{ |f| f.pilot_flights }.flatten
+known_flights = contests.map{ |c| c.flights.where(sequence: 1) }.flatten.compact
+# puts "known_flights: #{known_flights.count}"
+
+# Get all the PilotFlights for those Known flights
+pilot_flights = known_flights.map{ |kf| kf.pilot_flights }.flatten
 # puts "pilot_flights: #{pilot_flights.count}"
 
+# Get the Airplane record for each of those PilotFlights
 airplanes = pilot_flights.map{ |pf| pf.airplane }
 # puts "airplanes: #{airplanes.count}"
 
+# Get the Make/Model record for each of those Airplanes
+# Note: Some Airplane records don't have a corresponding Make/Model; in those cases,
+#       we use a dummy MakeModel records with 
 mms = airplanes.map{ |a| a&.make_model || unk }
 
 mms.map{ |mm| "#{u(mm.make)},#{u(mm.model)}" }.each do |key|
