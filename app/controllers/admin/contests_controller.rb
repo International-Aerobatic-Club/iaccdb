International-Aerobatic-Club/iaccdb

commit 425e3f812d2537865e61d6c43025fadec79c75c7

Changed `.order` methods to pure ARQI where possible, per issue #188.

app/controllers/admin/contests_controller.rb
@@ -6,7 +6,7 @@ class Admin::ContestsController < ApplicationController
   # GET /contests
   # GET /contests.xml
   def index
-    @contests = Contest.includes(:manny_synch).order("start DESC")
+    @contests = Contest.includes(:manny_synch).order(start: :desc")
   end
 
   # GET /contests/1/edit

app/controllers/contests_controller.rb
@@ -13,10 +13,10 @@ class ContestsController < ApplicationController
     @year = params[:year] || default_year(@years.first)
     @contests = Contest.where(
       'year(start) = ? and start <= now()', @year
-    ).order("start DESC").includes(:flights)
+    ).order(start: :desc).includes(:flights)
     @future_contests = Contest.where(
       'year(start) = ? and now() < start', @year
-    ).order("start ASC")
+    ).order(:start)
   end
 
   # GET /contests/1

app/controllers/admin/data_posts_controller.rb
@@ -2,7 +2,7 @@ class Admin::DataPostsController < ApplicationController
   before_action :authenticate
 
   def index
-    @data_posts = DataPost.order('created_at desc').all
+    @data_posts = DataPost.order(created_at: :desc).all
   end
 
   def show

app/controllers/admin/failures_controller.rb
@@ -2,7 +2,7 @@ class Admin::FailuresController < ApplicationController
   before_action :authenticate
 
   def index
-    @failures = Failure.includes(:contest).order("created_at DESC")
+    @failures = Failure.includes(:contest).order(created_at: :desc)
   end
 
   def show

app/controllers/admin/manny_synchs_controller.rb
@@ -4,7 +4,7 @@ class Admin::MannySynchsController < ApplicationController
   before_action :authenticate
 
   def index
-    @manny_synchs = MannySynch.includes(:contest).order("manny_number DESC")
+    @manny_synchs = MannySynch.includes(:contest).order(manny_number: :desc)
   end
 
   def destroy

app/controllers/admin/queues_controller.rb
@@ -2,7 +2,7 @@ class Admin::QueuesController < ApplicationController
   before_action :authenticate
 
   def index
-    @jobs = Delayed::Job.order('created_at DESC')
+    @jobs = Delayed::Job.order(created_at: :desc)
   end
 
   def show

app/controllers/further_controller.rb
@@ -35,8 +35,7 @@ class FurtherController < ApplicationController
   def airplane_make_model
     @years = Contest.joins(:flights => {:pilot_flights => :airplane}).select(
       "distinct year(start) as anum"
-      ).order('anum desc'
-      ).all.collect { |contest| contest.anum }
+      ).order(anum: :desc).all.collect { |contest| contest.anum }
     @year = params[:year] || @years.first
     airplanes_with_cat = Airplane.joins([
       {:pilot_flights => {:flight => [:categories, :contest]}},

app/controllers/hq/collegiate_results_controller.rb
@@ -92,7 +92,7 @@ class CollegiateResultsController < ApplicationController
   private
   ###
   def year_setup(params)
-    @years = CollegiateResult.select('distinct year').order('year desc')
+    @years = CollegiateResult.select('distinct `year`').order(year: :desc)
     @year = params[:year] || @years.first || Time.now.year
   end
 end

commit 5cd5d0cb294d87f57dd9c7d701aeaaeb207b80eb

Fixed a syntax error introduced in commit 425e3f812d2537865e61d6c43025fadec79c75c7

app/controllers/admin/contests_controller.rb
@@ -6,7 +6,7 @@ class Admin::ContestsController < ApplicationController
   # GET /contests
   # GET /contests.xml
   def index
-    @contests = Contest.includes(:manny_synch).order(start: :desc")
+    @contests = Contest.includes(:manny_synch).order(start: :desc)
   end
 
   # GET /contests/1/edit