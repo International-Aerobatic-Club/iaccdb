ad1c80feaffb27c3af1c735bdf019bc8b05e6375
djmolny <djmolny@gmail.com>
2022-10-11 10:22:30 -0600

    Updated 'development' configuration so that it logs which line of source code line invokes each ActiveRecord query.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Show the line of code that triggers each query
+  config.active_record.verbose_query_logs = true
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
