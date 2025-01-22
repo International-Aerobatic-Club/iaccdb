c6b0bcab07658734f93ab432f779e4afb83f12bd
djmolny <djmolny@gmail.com>
2025-01-21 18:43:15 -0700

    SRI: Fixed the code indentation; no functional changes.

--- app/models/collegiate_result.rb
+++ app/models/collegiate_result.rb
@@ -1,30 +1,30 @@
 class CollegiateResult < Result
 
-def to_s
-  "Collegiate Team #{name} #{year} #{result_percent}%"
-end
-
-def pilot_contests
-  members.map{ |member| [member, IAC::CollegiateComputer.pilot_results(member, year)] }.to_h
-end
-
-def self.find_or_create_team_for_year(team_name, year)
-  team = CollegiateResult.where(:name => team_name, :year => year).first
-  if (team == nil)
-    team = CollegiateResult.create(:name => team_name, :year => year)
+  def to_s
+    "Collegiate Team #{name} #{year} #{result_percent}%"
   end
-  team
-end
 
-def self.list_collegiate_for_year(year)
-  puts "#{year} Collegiate Teams Composition:"
-  teams = CollegiateResult.where(:year => year)
-  teams.each do |team|
-    puts team
-    team.members.each do |member|
-      puts "\t#{member}"
+  def pilot_contests
+    members.map{ |member| [member, IAC::CollegiateComputer.pilot_results(member, year)] }.to_h
+  end
+
+  def self.find_or_create_team_for_year(team_name, year)
+    team = CollegiateResult.where(:name => team_name, :year => year).first
+    if (team == nil)
+      team = CollegiateResult.create(:name => team_name, :year => year)
+    end
+    team
+  end
+
+  def self.list_collegiate_for_year(year)
+    puts "#{year} Collegiate Teams Composition:"
+    teams = CollegiateResult.where(:year => year)
+    teams.each do |team|
+      puts team
+      team.members.each do |member|
+        puts "\t#{member}"
+      end
     end
   end
-end
 
 end
