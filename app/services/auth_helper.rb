abf558d574e35d7d0f85dad35130b62f51d01d98
djmolny <djmolny@gmail.com>
2025-01-23 09:41:14 -0700

    Finished (?) implementing feature #275: Move secrets to config/credentials.yml.enc

--- app/services/auth_helper.rb
+++ app/services/auth_helper.rb
@@ -1,12 +1,15 @@
 module AuthHelper
+
+  include ActiveSupport
+
   def current_role
     @current_role ||= :visitor
   end
 
   def check_auth(user_name, password, role = :admin)
     user = UserList.find_by_name(user_name)
-    have_auth = user && user['password'] == password
-    @current_role = have_auth ? user['role'].to_sym : :visitor
+    have_auth = user && user[:password] == password
+    @current_role = have_auth ? user[:role].to_sym : :visitor
     roles = [:admin] + [role].flatten.compact.map(&:to_sym)
     have_auth && roles.member?(current_role)
   end
@@ -16,20 +19,13 @@ module AuthHelper
       attr_accessor :users
 
       def users
-        @users ||=
-          Rails.application.secrets['users'] ||
-          YAML.load_file('config/admin.yml')['users'] ||
-          []
+        Rails.application.credentials.dig(:users)
       end
 
       def find_by_name(user_name)
-        users.find { |u| u['name'] == user_name }
+        Rails.application.credentials.dig(:users, user_name.to_sym)
       end
 
-      def first_with_role(role)
-        role = role ? role.to_sym : :admin
-        users.find { |u| u['role'].to_sym == role }
-      end
     end
   end
 end
