b0713b2a17ae6dbe7726876b00f00e61a679ab0c
djmolny <djmolny@gmail.com>
2025-01-25 08:59:42 -0700

    SRI: Rewrote long SQL queries with Rails ARQI (https://guides.rubyonrails.org/v5.2/active_record_querying.html)

--- app/controllers/pilots_controller.rb
+++ app/controllers/pilots_controller.rb
@@ -1,23 +1,16 @@
 class PilotsController < ApplicationController
   def index
-    @pilots = Member.find_by_sql("select 
-      m.given_name, m.family_name, m.id
-      from members m where m.id in (select distinct pilot_id from pilot_flights)
-      order by  m.family_name, m.given_name")
+    # Joining with `pilot_flights` acts as a filter to select only those Members who have competed
+    @pilots = Member.joins(:pilot_flights).order(:family_name, :given_name).distinct
   end
 
   def show
     id = params[:id]
     @pilot = Member.find(id)
-    @contests = Contest.find_by_sql(["select distinct
-      c.id, c.name, c.start, c.region, t.name as category
-      from pilot_flights p, members m, flights f, contests c,
-           categories_flights j, categories t
-      where p.pilot_id = :id
-        and p.flight_id = f.id
-        and f.contest_id = c.id
-        and j.flight_id = f.id
-        and t.id = j.category_id
-      order by c.start desc", {:id => id}])
+    @contests =
+      Contest.joins(flights: :pilot_flights)
+             .where(flights: { pilot_flights: { pilot_id: id }})
+             .order(start: :desc)
+             .distinct
   end
 end
