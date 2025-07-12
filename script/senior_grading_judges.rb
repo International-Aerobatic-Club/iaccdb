292ad0d81ace07e8c54f10559b446d80b0c6ef1d
IAC Webmaster <DJ Molny>
2025-07-12 13:01:34 -0600

    Added a filter to only select judges who have served within the past three calendar years. Added comments. Moved literal values into constants.

--- script/senior_grading_judges.rb
+++ script/senior_grading_judges.rb
@@ -1,31 +1,65 @@
 require 'csv'
 
 
-members = Hash.new
+MIN_FLIGHTS=250
+RECENT_WINDOW = 3 # years
 
 
+# Key = Member#id, Value = Number of flights graded.
+judges = Hash.new
+
+# Key = Member#id, Value = boolean
+recent = Hash.new
+
+# Get all Flight#id values for all contests within the past three calendar years
+recent_flight_ids = Flight.where(contest: Contest.where('YEAR(start) >= ?', Time.now.year - RECENT_WINDOW)).pluck(:id)
+
+
+# Tally the number of flights that each member line-judged
 JfResult.includes(:judge).find_in_batches do |batch|
+
   batch.each do |jf_result|
-    mid = jf_result.judge.judge_id
-    members[mid] ||= 0
-    members[mid] += 1
+
+    # Convenience var
+    jid = jf_result.judge.judge_id
+
+    # Initialize the Hash entry if necessary, then increment
+    judges[jid] ||= 0
+    judges[jid] += 1
+
+    # Note whether the flight was within the RECENT_WINDOW
+    recent[jid] = true if recent_flight_ids.include? jf_result.flight_id
+
   end
+
 end
 
 
+# Now tally the Chief Judges similarly
 Flight.find_in_batches do |batch|
+
   batch.each do |flight|
-    mid = flight.chief_id || next
-    members[mid] ||= 0
-    members[flight.chief_id] += flight.pilot_flights.count
+
+    # Convenience var, but skip this record if no Chief is listed
+    jid = flight.chief_id || next
+    judges[jid] ||= 0
+    judges[flight.chief_id] += flight.pilot_flights.count
+    recent[jid] = true if recent_flight_ids.include? flight.id
+
   end
+
 end
 
 
+# Filter and output the results
 CSV($stdout) do |csv|
-  members
-    .find_all{ |k,v| v > 250 }
-    .map{ |mid,v| ["#{Member.find(mid).given_name} #{Member.find(mid).family_name}", mid, v] }
+
+  judges
+    # Filter out judges who haven't judged within RECENT_WINDOW or have less than MIN_FLIGHTS flights judged/chiefed
+    .find_all{ |k,v| recent[k] && v >= MIN_FLIGHTS }
+    # Produce an array of CSV columns
+    .map{ |jid,v| ["#{Member.find(jid).given_name} #{Member.find(jid).family_name}", jid, v] }
     .sort
-    .each{ |member_name, mid, count| csv << [member_name, Member.find(mid).iac_id, count] }
+    .each{ |cols| csv << cols }
+
 end
