647cdd0d6d4f4eb3e30e4a0dd10870fdc3e312ac
djmolny <djmolny@gmail.com>
2025-11-02 13:12:51 -0700

    Fixed issue #305: bad validation

--- app/models/free_program_k.rb
+++ app/models/free_program_k.rb
@@ -2,7 +2,7 @@ class FreeProgramK < ApplicationRecord
 
   belongs_to :category
 
-  validates :year, numericality: { only_integer: true, greater_than: 2005 }
+  validates :year, numericality: { only_integer: true, greater_than: 2004 }
   validates :category_id, uniqueness: { scope: :year, message: "already has a max K factor for that year" }
   validates_presence_of :category_id
   validates :max_k, numericality: { only_integer: true, greater_than: 0 }
