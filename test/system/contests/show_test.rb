2bff031e3b62c8d64fa6ac70469a852636412626
Dillon TenBrink <dillon@tenbrink.us>
2026-04-27 20:28:05 -0500

    display collegiate participants in contest results Fixes #9

--- test/system/contests/show_test.rb
+++ test/system/contests/show_test.rb
@@ -38,4 +38,30 @@ class ContestsShowTest < ActionDispatch::IntegrationTest
     assert_select('p.contest_updated',
       "Last Updated: #{contest.reload.updated_at.strftime('%m/%d/%Y %H:%M %Z')}")
   end
+
+  test "contest marks collegiate participants" do
+    contest = create(:contest, start: Date.new(2024, 6, 1))
+    category = create(:category)
+    flight = create(:flight, contest: contest, category_id: category.id)
+    collegiate_pilot = create(:member, given_name: 'Casey', family_name: 'College')
+    regular_pilot = create(:member, given_name: 'Riley', family_name: 'Regular')
+
+    [collegiate_pilot, regular_pilot].each_with_index do |pilot, index|
+      create(:pc_result, contest: contest, category: category, pilot: pilot,
+        category_rank: index + 1, category_value: 1000 - index, total_possible: 1200)
+      pilot_flight = create(:pilot_flight, flight: flight, pilot: pilot)
+      create(:pf_result, pilot_flight: pilot_flight,
+        adj_flight_rank: index + 1, adj_flight_value: 1000 - index,
+        total_possible: 1200)
+    end
+
+    team = CollegiateResult.create!(name: 'UND', year: contest.year)
+    team.members << collegiate_pilot
+
+    get contest_path(contest.id)
+
+    assert_select('span.collegiate-participant', count: 1)
+    assert_select('span.collegiate-participant[title=?]',
+      'Collegiate Series participant: UND')
+  end
 end
