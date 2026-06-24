e8521c2f05661315f15d2398a2587cc7039e36b6
djmolny <djmolny@gmail.com>
2026-06-24 09:09:11 -0600

    Fixed indentation; no functional changes.

--- app/services/jobs/compute_collegiate_job.rb
+++ app/services/jobs/compute_collegiate_job.rb
@@ -1,37 +1,39 @@
 # This captures a job for delayed job
 # The job computes collegiate results for the contest year
 module Jobs
-class ComputeCollegiateJob < Struct.new(:contest)
 
-  include JobsSay
+  class ComputeCollegiateJob < Struct.new(:contest)
 
-  def perform
-    @year = contest.year
-    @description = "#{@year}"
-    if (contest.year == Time.now.year)
-      make_computation
-    else
-      say "Skipping collegiate computation for year, #{@year}"
+    include JobsSay
+
+    def perform
+      @year = contest.year
+      @description = "#{@year}"
+      if (contest.year == Time.now.year)
+        make_computation
+      else
+        say "Skipping collegiate computation for year, #{@year}"
+      end
     end
+
+    def make_computation
+      say "Computing collegiate standings for #{@description}"
+      computer.recompute
+    end
+
+    def computer
+      Iac::CollegiateComputer.new(@year)
+    end
+
+    def error(job, exception)
+      say "Error collegiate computation #{@description}"
+      record_contest_failure(@description, contest, exception)
+    end
+
+    def success(job)
+      say "Success collegiate computation #{@description}"
+    end
+
   end
 
-  def make_computation
-    say "Computing collegiate standings for #{@description}"
-    computer.recompute
-  end
-
-  def computer
-    Iac::CollegiateComputer.new(@year)
-  end
-
-  def error(job, exception)
-    say "Error collegiate computation #{@description}"
-    record_contest_failure(@description, contest, exception)
-  end
-
-  def success(job)
-    say "Success collegiate computation #{@description}"
-  end
-
-end
 end
