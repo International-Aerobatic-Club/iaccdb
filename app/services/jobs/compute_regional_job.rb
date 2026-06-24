e031feabc25677f965f252b91c435914768f5a55
djmolny <djmolny@gmail.com>
2026-06-24 09:09:37 -0600

    Fixed indentation; no functional changes.

--- app/services/jobs/compute_regional_job.rb
+++ app/services/jobs/compute_regional_job.rb
@@ -1,34 +1,36 @@
 # This captures a job for delayed job
 # The job computes regional series results for a given region and year
 module Jobs
-class ComputeRegionalJob < Struct.new(:contest)
 
-  include JobsSay
+  class ComputeRegionalJob < Struct.new(:contest)
 
-  def perform
-    @contest = contest
-    @description = "Region #{contest.region}, year #{contest.year}"
-    if (contest.year == Time.now.year)
-      make_computation
-    else
-      say "Skipping regional for #{@description} not current year"
+    include JobsSay
+
+    def perform
+      @contest = contest
+      @description = "Region #{contest.region}, year #{contest.year}"
+      if (contest.year == Time.now.year)
+        make_computation
+      else
+        say "Skipping regional for #{@description} not current year"
+      end
     end
+
+    def make_computation
+      say "Computing regional series standings for #{@description}"
+      series = Iac::RegionalSeries.new
+      series.compute_regional_for_contest(contest)
+    end
+
+    def error(job, exception)
+      say "Error regional series computation #{@description}"
+      record_contest_failure(@description, @contest, exception)
+    end
+
+    def success(job)
+      say "Success regional series computation #{@description}"
+    end
+
   end
 
-  def make_computation
-    say "Computing regional series standings for #{@description}"
-    series = Iac::RegionalSeries.new
-    series.compute_regional_for_contest(contest) 
-  end
-
-  def error(job, exception)
-    say "Error regional series computation #{@description}"
-    record_contest_failure(@description, @contest, exception)
-  end
-
-  def success(job)
-    say "Success regional series computation #{@description}"
-  end
-
-end
 end
